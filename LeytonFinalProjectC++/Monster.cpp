61d269305e80b3d95a6cabff172af863bc4e903a
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-11 15:23:53 +0000

    Added formulas to calculate actual stat of monster

--- LeytonFinalProjectC++/Monster.cpp
+++ LeytonFinalProjectC++/Monster.cpp
@@ -17,6 +17,14 @@ public:
 	int specialDefenceBase;
 	int speedBase;
 
+	int healthActual;
+	int physcialAttackActual;
+	int physicalDefenceActual;
+	int specialAttackActual;
+	int specialDefenceActual;
+	int speedActual;
+
+	int iv;
 	int level;
 	float experience;
 	float nextExperienceNeeded;
@@ -36,7 +44,37 @@ public:
 		specialDefenceBase = _specialDefenceBase;
 		speedBase = _speedBase;
 		level = _level;
+		iv = generateRandomIV();
+		calculateActualStatistics();
 	}
+
+	void calculateActualStatistics() {
+		healthActual = healthFormula(healthBase);
+		physcialAttackActual = otherStatisticFormula(physcialAttackBase);
+		physicalDefenceActual = otherStatisticFormula(physicalDefenceBase);
+		specialAttackActual = otherStatisticFormula(specialAttackBase);
+		specialDefenceActual = otherStatisticFormula(specialDefenceBase);
+		speedActual = otherStatisticFormula(speedBase);
+	}
+
+	int healthFormula(int stat) {
+		return floor(((((2* stat)+iv )* level) / 100) + level + 10);
+	}
+
+	int otherStatisticFormula(int stat) {
+		return floor(((((2 * stat) + iv) * level) / 100) + 5);
+	}
+
+	int generateRandomIV() {
+		return (rand() % 31) + 10;
+	}
+
+	
+
+
+
+
+
 
 
 
