d090e2b4cd9c464252a3a6ab57ed184d0c50056d
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-11 14:39:19 +0000

    Added the code to the wrong file

--- LeytonFinalProjectC++/Monster.h
+++ LeytonFinalProjectC++/Monster.h
@@ -1,45 +1,5 @@
-#include <string>
-#include <SFML/Graphics.hpp>
-using namespace std;
-using namespace sf;
-
+#pragma once
 class Monster
 {
-
-public:
-
-	string monsterName;
-
-	int healthBase;
-	int physcialAttackBase;
-	int physicalDefenceBase;
-	int specialAttackBase;
-	int specialDefenceBase;
-	int speedBase;
-
-	int level;
-	float experience;
-	float nextExperienceNeeded;
-
-	Sprite monserSprite;
-
-	Monster(string _monsterName, int _healthBase, int _physcialAttackBase, int _physicalDefenceBase, int _specialAttackBase, int _specialDefenceBase, int _speedBase, int _level) {
-		monsterName = _monsterName;
-		healthBase = _healthBase;
-		physcialAttackBase = _physcialAttackBase;
-		physicalDefenceBase = _physicalDefenceBase;
-		specialAttackBase = _specialAttackBase;
-		specialDefenceBase = _specialDefenceBase;
-		speedBase = _speedBase;
-		level = _level;
-	}
-
-	void calculateActualStats() {
-
-	}
-
-	
-
-
 };
 
