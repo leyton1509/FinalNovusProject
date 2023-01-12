eaba8e2e953d816e8fd8aaf0989fc8fb58934c67
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-12 10:46:45 +0000

    Working black screen, needs linking libraries for font

--- LeytonFinalProjectC++/Main.cpp
+++ LeytonFinalProjectC++/Main.cpp
@@ -27,13 +27,46 @@ int main()
     ALLEGRO_DISPLAY* disp = al_create_display(1200, 800);
 
     // Changes font
-    ALLEGRO_FONT* font = al_create_builtin_font();
+    //ALLEGRO_FONT* font = al_create_builtin_font();
 
 
     // Register what events im interested in
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(disp));
     al_register_event_source(queue, al_get_timer_event_source(timer));
+
+
+    al_start_timer(timer);
+    while (1)
+    {
+
+        bool redraw = true;
+        ALLEGRO_EVENT event;
+
+        al_wait_for_event(queue, &event);
+
+        if (event.type == ALLEGRO_EVENT_TIMER)
+            redraw = true;
+        else if ((event.type == ALLEGRO_EVENT_KEY_DOWN) || (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE))
+            break;
+
+
+        if (redraw && al_is_event_queue_empty(queue))
+        {
+            // Sets screen to black
+            al_clear_to_color(al_map_rgb(0, 0, 0));
+            // Draws tge text in top left 
+            //al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "Hello world!");
+            // Commits results
+            al_flip_display();
+            redraw = false;
+        }
+    }
+
+    //al_destroy_font(font);
+    al_destroy_display(disp);
+    al_destroy_timer(timer);
+    al_destroy_event_queue(queue);
 
     return 0;
 }
