0c6e669bbdf476898cf42f3cc6ead861c495e055
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-12 15:43:46 +0000

    Added empty handles for mouse and keyboards input

--- LeytonFinalProjectC++/Main.cpp
+++ LeytonFinalProjectC++/Main.cpp
@@ -28,6 +28,7 @@ int main()
 {
     must_init(al_init(), "allegro");
     must_init(al_install_keyboard(), "keyboard");
+    must_init(al_install_mouse(), "mouse");
 
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0);
     must_init(timer, "timer");
@@ -47,6 +48,7 @@ int main()
     //ALLEGRO_BITMAP* mysha = al_load_bitmap("mysha.png");
     //must_init(mysha, "mysha");
 
+    al_register_event_source(queue, al_get_mouse_event_source());
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(disp));
     al_register_event_source(queue, al_get_timer_event_source(timer));
@@ -54,6 +56,9 @@ int main()
     bool done = false;
     bool redraw = true;
     ALLEGRO_EVENT event;
+
+    double xMousePosition = 0;
+    double yMousePosition = 0;
 
     al_start_timer(timer);
     while (1)
@@ -67,7 +72,28 @@ int main()
             redraw = true;
             break;
 
-        case ALLEGRO_EVENT_KEY_DOWN:
+        // Handle mouse movement
+        case ALLEGRO_EVENT_MOUSE_AXES:
+            xMousePosition = event.mouse.x;
+            yMousePosition = event.mouse.y;
+            break;
+
+        // Handle mouse click
+        case ALLEGRO_EVENT_MOUSE_BUTTON_UP:
+            break;
+
+        // Handle key board input
+         ALLEGRO_KEYBOARD_STATE keyState;
+         al_get_keyboard_state(&keyState);
+
+         if (al_key_down(&keyState, ALLEGRO_KEY_W)) {
+             // Handles two buttons at once
+             if (al_key_down(&keyState, ALLEGRO_KEY_LCTRL)) {
+
+             }
+         }
+
+
         case ALLEGRO_EVENT_DISPLAY_CLOSE:
             done = true;
             break;
@@ -92,7 +118,8 @@ int main()
 
     al_uninstall_keyboard();
     //al_destroy_bitmap(mysha);
-    //al_destroy_font(font);
+    al_destroy_font(font);
+    al_ungrab_mouse();
     al_destroy_display(disp);
     al_destroy_timer(timer);
     al_destroy_event_queue(queue);
