f448101ecf52851df6bbbba29a063fe4a5f75b18
taylor92 <taylor92@lancaster.ac.uk>
2023-01-13 17:53:58 +0000

    Charcter will now move based on framerate and direction

--- LeytonFinalProjectC++/Sprites/PlayerCharacter.cpp
+++ LeytonFinalProjectC++/Sprites/PlayerCharacter.cpp
@@ -5,66 +5,97 @@
 #include <string>
 
 
-#include <direct.h>
-#include<limits.h>
-#include<iostream>
 
 using namespace std;
-
-
 
 
  class PlayerCharacter : public Sprite{
 
  public:
 
+	 int xValueForStyleSheet;
+	 int yValueForStyleSheet;
+	 int moveBy = 8;
+	 int state = 0;
 
-
-	PlayerCharacter() : Sprite(SpriteType::SpriteTypes::Player, 0, 0, 0, 0, 0, -1, 64, 64, "Sprites/PlayerSprites/PlayerCharacterSpriteSheet.png") {
+	PlayerCharacter() : Sprite(SpriteType::SpriteTypes::Player, 0, 0, 0, 0, 0, 0, 64, 64, "Sprites/PlayerSprites/PlayerCharacterSpriteSheet.png") {
+		xValueForStyleSheet = 0;
+		yValueForStyleSheet = 0;
 	}
 
 	void moveCharacter(const char * keyPressed) {
 
-		if (strcmp(keyPressed, "w") || strcmp(keyPressed, "up") == 0) {
+
+		if (strcmp(keyPressed, "w") == 0 ){
 			if (directionY!=1) {
-				al_draw_bitmap_region(spriteImage, 0, 0, 256, 64, xPosition, yPosition, 0);
+				yValueForStyleSheet = 192;
+				state = 0;
 				directionY = 1;
 				directionX = 0;
 			}
+			else {
+				state = 1;
+			}
+
+			yPosition -= moveBy;
 		}
 
-		if (strcmp(keyPressed, "s") || strcmp(keyPressed, "down") == 0) {
+		else if (strcmp(keyPressed, "s") == 0 ){
 			if (directionY != -1) {
-				al_draw_bitmap_region(spriteImage, 0, 0, 64, 64, xPosition, yPosition, 0);
+				yValueForStyleSheet = 0;
+				state = 0;
 				directionY = -1;
-				directionX = 0;
+				directionX = 0;		
 			}
+			else {
+				state = 1;
+			}
+			yPosition += moveBy;
 		}
 
-		if (strcmp(keyPressed, "a") || strcmp(keyPressed, "left") == 0) {
-			if (directionY != -1) {
-				al_draw_bitmap_region(spriteImage, 0, 0, 128, 64, xPosition, yPosition, 0);
+		else if (strcmp(keyPressed, "a") == 0) {
+			if (directionX != -1) {
+				yValueForStyleSheet = 64;
+				state = 0;
 				directionX = -1;
 				directionY = 0;
 			}
+			else {
+				state = 1;
+			}
+			xPosition -= moveBy;
 		}
 
-		if (strcmp(keyPressed, "d") || strcmp(keyPressed, "right") == 0) {
-			if (directionY != -1) {
-				al_draw_bitmap_region(spriteImage, 0, 0, 192, 64, xPosition, yPosition, 0);
-				directionX = -1;
+		else if (strcmp(keyPressed, "d") == 0) {
+
+			if (directionX != 1) {
+				state = 0;
+				yValueForStyleSheet = 128;
+				directionX = 1;
 				directionY = 0;
 			}
+			else {
+				state = 1;
+			}
+
+			xPosition += moveBy;
 		}
 
 	}
 
 
 
-
-
 	void drawSprite() {
-		al_draw_bitmap_region(spriteImage, 0, 0, 64, 64, xPosition, yPosition, 0);
+		al_draw_bitmap_region(spriteImage, (64 * state), yValueForStyleSheet, 64, 64, xPosition, yPosition, 0);
+		if (state!=0) {
+			if (state == 3) {
+				state = 0;
+			}
+			else {
+				state++;
+			}
+		}
+		
 	}
 	
 	
