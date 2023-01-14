6b45a9cf9e1e4003c0884197748ad6bb0bd447a5
taylor92 <taylor92@lancaster.ac.uk>
2023-01-14 09:43:45 +0000

    Added comments

--- LeytonFinalProjectC++/Sprites/Sprite.cpp
+++ LeytonFinalProjectC++/Sprites/Sprite.cpp
@@ -3,24 +3,35 @@
 #include <allegro5/allegro_image.h>
 #include "SpriteType.cpp";
 
-
+// Class to represent a sprite 
 class Sprite
 {
 
+// Public accessors
 public:
+	
+	// Enum of the type of sprite
 	enum SpriteType::SpriteTypes typeOfSprite;
+	// The x position of the sprite
 	float xPosition;
+	// The y position of the sprite
 	float yPosition;
+	// The x velocity of the sprite
 	float velocityX;
+	// The y velocity of the sprite
 	float velocityY;
+	// The x direction of the sprite
 	int directionX;
+	// The y direction of the sprite
 	int directionY;
-
+	// Width of sprite
 	int spritewidth;
+	// Height of sprite
 	int spriteHeight;
-
+	// Bit map image of sprite or spritesheet
 	ALLEGRO_BITMAP* spriteImage;
 
+	// Constructor for sprite with inputs for speed and direction
 	Sprite(SpriteType::SpriteTypes _typeOfSprite,float _startX, float _startY, float _velocityX, float _velocityY, int _directionX, int _directionY, int _spritewidth, int _spriteHeight, const char* filename) {
 		typeOfSprite = _typeOfSprite;
 		xPosition = _startX;
@@ -35,6 +46,7 @@ public:
 
 	}
 
+	// Constructor for sprite without inputs for speed and direction
 	Sprite(SpriteType::SpriteTypes _typeOfSprite, float _startX, float _startY, int _spritewidth, int _spriteHeight, const char* filename) {
 		typeOfSprite = _typeOfSprite;
 		xPosition = _startX;
