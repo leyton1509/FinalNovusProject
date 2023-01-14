eedf07d55f06396895b72e9628d2416661457185
taylor92 <taylor92@lancaster.ac.uk>
2023-01-14 09:59:13 +0000

    Changed const to public

--- LeytonFinalProjectC++/Sprites/PokemonGrass.cpp
+++ LeytonFinalProjectC++/Sprites/PokemonGrass.cpp
@@ -3,7 +3,11 @@
 
 class PokemonGrass : public Sprite {
 
+public:
+
 	PokemonGrass(float _startXPosition, float _startYPosition) : Sprite(SpriteType::SpriteTypes::Grass, _startXPosition, _startYPosition, 32, 32, "Sprites/PlayerSprites/PlayerCharacterSpriteSheet.png") {
 	}
+
+
 
 };
