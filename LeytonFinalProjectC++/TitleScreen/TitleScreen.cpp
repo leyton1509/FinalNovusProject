d0d5210e00f3d580d67426ad822f18c3d5ae59cb
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-16 22:45:43 +0000

    Fixed issue with image, changed frame times to make pokemon glow

--- LeytonFinalProjectC++/TitleScreen/TitleScreen.cpp
+++ LeytonFinalProjectC++/TitleScreen/TitleScreen.cpp
@@ -19,7 +19,7 @@ public:
 	TitleScreen(int _screenWidth, int _screenHeight) {
 		currentFrame = 0;
 		pokemonText = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/PokemonTitle.png");
-		dialgaBasic = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/Dialga.png");
+		dialgaBasic = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/DialgaNone.png");
 		dialgaGlowing = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/DialgaPulse.png");
 		background = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/BackgroundTitle.jpg");
 		screenWidth = _screenWidth;
@@ -27,23 +27,28 @@ public:
 		hasClicked = false;
 	}
 
+	
+
 	void drawScreen() {
 		if (hasClicked == false) {
 			al_draw_scaled_bitmap(background, 0, 0, 728, 410, 0, 0, screenWidth, screenHeight, 0);
 			al_draw_scaled_bitmap(pokemonText, 0, 0, 493, 225, 0, 0, screenWidth * 0.8, screenHeight * 0.4, 0);
 
-			if (currentFrame >= 360) {
+			if (currentFrame >= 200) {
 				al_draw_scaled_bitmap(dialgaGlowing, 0, 0, 600, 600, screenWidth * 0.3, screenHeight * 0.55, screenWidth / 1.9, screenHeight / 2, 0);
-				if (currentFrame > 390) {
+				currentFrame++;
+				if (currentFrame > 210) {
 					currentFrame = 0;
 				}
 			}
 			else {
-				al_draw_scaled_bitmap(dialgaGlowing, 0, 0, 600, 600, screenWidth * 0.3, screenHeight * 0.55, screenWidth / 1.9, screenHeight / 2, 0);
+				al_draw_scaled_bitmap(dialgaBasic, 0, 0, 600, 600, screenWidth * 0.3, screenHeight * 0.55, screenWidth / 1.9, screenHeight / 2, 0);
 				currentFrame++;
 			}
 		}
 		else {
+
+
 
 		}
 		
