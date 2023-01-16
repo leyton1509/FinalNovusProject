351f557ff9c633ae1743d09f1bfda7502af3453c
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-16 21:43:03 +0000

    Added a name and the two basic sprites

--- LeytonFinalProjectC++/TitleScreen/TitleScreen.cpp
+++ LeytonFinalProjectC++/TitleScreen/TitleScreen.cpp
@@ -6,7 +6,18 @@ class TitleScreen {
 
 public:
 
-	
+	std::string name;
+
+	ALLEGRO_BITMAP * dialgaBasic;
+	ALLEGRO_BITMAP * dialgaGlowing;
+
+	TitleScreen(int _screenWidth, int _screenHeight) {
+		name = "Pokemon Shattered Prism";
+		dialgaBasic = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/Dialga.png");
+		dialgaGlowing = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/DialgaPulse.png");
+
+
+	}
 
 
 
