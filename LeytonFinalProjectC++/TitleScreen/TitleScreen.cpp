b04b1f619fda31d81940de45515a51d2db634d32
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-16 22:32:18 +0000

    Added pokemon text to the title screen

--- LeytonFinalProjectC++/TitleScreen/TitleScreen.cpp
+++ LeytonFinalProjectC++/TitleScreen/TitleScreen.cpp
@@ -1,4 +1,4 @@
-#include <string>
+
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_image.h>
 
@@ -6,7 +6,7 @@ class TitleScreen {
 
 public:
 
-	std::string name;
+	ALLEGRO_BITMAP * pokemonText;
 	ALLEGRO_BITMAP * dialgaBasic;
 	ALLEGRO_BITMAP * dialgaGlowing;
 	ALLEGRO_BITMAP * background;
@@ -16,7 +16,7 @@ public:
 	int screenHeight;
 
 	TitleScreen(int _screenWidth, int _screenHeight) {
-		name = "Pokemon Shattered Prism";
+		pokemonText = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/PokemonTitle.png");
 		dialgaBasic = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/Dialga.png");
 		dialgaGlowing = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/DialgaPulse.png");
 		background = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/BackgroundTitle.jpg");
@@ -26,15 +26,16 @@ public:
 
 	void drawFirstScreen() {
 		al_draw_scaled_bitmap(background, 0, 0, 728, 410, 0,0, screenWidth, screenHeight, 0);
+		al_draw_scaled_bitmap(pokemonText, 0, 0, 493, 225, 0, 0, screenWidth * 0.8, screenHeight * 0.4, 0);
 
 		if (currentFrame >= 360) {
-			al_draw_scaled_bitmap(dialgaGlowing, 0, 0, 600, 600, screenWidth * 0.5, screenHeight * 0.55, screenWidth / 1.9, screenHeight / 2, 0);
+			al_draw_scaled_bitmap(dialgaGlowing, 0, 0, 600, 600, screenWidth * 0.3, screenHeight * 0.55, screenWidth / 1.9, screenHeight / 2, 0);
 			if (currentFrame > 390) {
 				currentFrame = 0;
 			}
 		}
 		else {
-			al_draw_scaled_bitmap(dialgaGlowing, 0, 0, 600, 600, screenWidth * 0.5, screenHeight * 0.55, screenWidth / 1.9, screenHeight / 2, 0);
+			al_draw_scaled_bitmap(dialgaGlowing, 0, 0, 600, 600, screenWidth * 0.3, screenHeight * 0.55, screenWidth / 1.9, screenHeight / 2, 0);
 			currentFrame++;
 		}
 		
