e9f3900c898499159867529c7d627be5e4ea7ed0
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-16 22:37:27 +0000

    Added a has clicked check for drawing the scene, as the next part will be creating / loading a game

--- LeytonFinalProjectC++/TitleScreen/TitleScreen.cpp
+++ LeytonFinalProjectC++/TitleScreen/TitleScreen.cpp
@@ -28,18 +28,23 @@ public:
 	}
 
 	void drawScreen() {
-		al_draw_scaled_bitmap(background, 0, 0, 728, 410, 0,0, screenWidth, screenHeight, 0);
-		al_draw_scaled_bitmap(pokemonText, 0, 0, 493, 225, 0, 0, screenWidth * 0.8, screenHeight * 0.4, 0);
+		if (hasClicked == false) {
+			al_draw_scaled_bitmap(background, 0, 0, 728, 410, 0, 0, screenWidth, screenHeight, 0);
+			al_draw_scaled_bitmap(pokemonText, 0, 0, 493, 225, 0, 0, screenWidth * 0.8, screenHeight * 0.4, 0);
 
-		if (currentFrame >= 360) {
-			al_draw_scaled_bitmap(dialgaGlowing, 0, 0, 600, 600, screenWidth * 0.3, screenHeight * 0.55, screenWidth / 1.9, screenHeight / 2, 0);
-			if (currentFrame > 390) {
-				currentFrame = 0;
+			if (currentFrame >= 360) {
+				al_draw_scaled_bitmap(dialgaGlowing, 0, 0, 600, 600, screenWidth * 0.3, screenHeight * 0.55, screenWidth / 1.9, screenHeight / 2, 0);
+				if (currentFrame > 390) {
+					currentFrame = 0;
+				}
+			}
+			else {
+				al_draw_scaled_bitmap(dialgaGlowing, 0, 0, 600, 600, screenWidth * 0.3, screenHeight * 0.55, screenWidth / 1.9, screenHeight / 2, 0);
+				currentFrame++;
 			}
 		}
 		else {
-			al_draw_scaled_bitmap(dialgaGlowing, 0, 0, 600, 600, screenWidth * 0.3, screenHeight * 0.55, screenWidth / 1.9, screenHeight / 2, 0);
-			currentFrame++;
+
 		}
 		
 	}
