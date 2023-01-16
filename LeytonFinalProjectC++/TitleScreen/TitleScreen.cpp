ba9e95a1d9c76efc580bd7ffeda60c3daee86d8c
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-16 22:23:34 +0000

    Added the corect dimensions for drawings

--- LeytonFinalProjectC++/TitleScreen/TitleScreen.cpp
+++ LeytonFinalProjectC++/TitleScreen/TitleScreen.cpp
@@ -7,8 +7,6 @@ class TitleScreen {
 public:
 
 	std::string name;
-
-
 	ALLEGRO_BITMAP * dialgaBasic;
 	ALLEGRO_BITMAP * dialgaGlowing;
 	ALLEGRO_BITMAP * background;
@@ -19,25 +17,24 @@ public:
 
 	TitleScreen(int _screenWidth, int _screenHeight) {
 		name = "Pokemon Shattered Prism";
-		dialgaBasic = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/Dialga.png");
+		dialgaBasic = al_load_bitmap("C:/Users/leyto/source/repos/LeytonFinalProjectC++/LeytonFinalProjectC++/Sprites/TitleScreenSprites/Dialga.png");
 		dialgaGlowing = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/DialgaPulse.png");
-		background = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/BackgrondTitle.jpg");
+		background = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/BackgroundTitle.jpg");
 		screenWidth = _screenWidth;
 		screenHeight = _screenHeight;
 	}
 
 	void drawFirstScreen() {
-		al_draw_bitmap(background, 0, 0, 0);
+		al_draw_scaled_bitmap(background, 0, 0, 728, 410, 0,0, screenWidth, screenHeight, 0);
 
 		if (currentFrame >= 360) {
-			al_draw_bitmap(dialgaGlowing, screenWidth * 0.6, screenHeight *0.4, 0);
+			al_draw_scaled_bitmap(dialgaGlowing, 0, 0, 600, 600, screenWidth * 0.5, screenHeight * 0.55, screenWidth / 1.9, screenHeight / 2, 0);
 			if (currentFrame > 390) {
 				currentFrame = 0;
 			}
-		
 		}
 		else {
-			al_draw_bitmap(dialgaBasic, screenWidth * 0.6, screenHeight * 0.4, 0);
+			al_draw_scaled_bitmap(dialgaGlowing, 0, 0, 600, 600, screenWidth * 0.5, screenHeight * 0.55, screenWidth / 1.9, screenHeight / 2, 0);
 			currentFrame++;
 		}
 		
