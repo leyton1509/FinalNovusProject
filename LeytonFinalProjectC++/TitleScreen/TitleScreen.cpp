be4c9f5f8940ecf669cb9d80f7a78e0518b0ee62
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-17 12:08:31 +0000

    Changed the bitmap to a button sprite

--- LeytonFinalProjectC++/TitleScreen/TitleScreen.cpp
+++ LeytonFinalProjectC++/TitleScreen/TitleScreen.cpp
@@ -1,6 +1,7 @@
 
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_image.h>
+#include "../Sprites/Button.h"
 
 class TitleScreen {
 
@@ -11,16 +12,16 @@ public:
 	ALLEGRO_BITMAP * dialgaGlowing;
 	ALLEGRO_BITMAP * background;
 
-	ALLEGRO_BITMAP* newGameButton;
-	ALLEGRO_BITMAP* loadGameButton;
+	Button newGameButton;
+	Button loadGameButton;
 
 	int currentFrame;
 	int screenWidth;
 	int screenHeight;
-	bool hasClicked;
-	bool hasLoadedButtons;
+	bool hasClicked = NULL;
+	bool hasLoadedButtons = NULL;
 
-	TitleScreen(int _screenWidth, int _screenHeight) {
+	TitleScreen(int _screenWidth, int _screenHeight){
 		currentFrame = 0;
 		pokemonText = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/PokemonTitle.png");
 		dialgaBasic = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/DialgaNone.png");
@@ -53,8 +54,9 @@ public:
 		}
 		else {
 			if (hasLoadedButtons == false) {
-				newGameButton = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/NewGameButton.jpg");
-				loadGameButton = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/LoadGameButton.jpg");
+
+				newGameButton = Button(281, 60, (screenWidth * 0.5 - screenWidth * 0.35), (screenHeight * 0.3), (screenWidth * 0.7), (screenHeight * 0.2), "../LeytonFinalProjectC++/Sprites/TitleScreenSprites/NewGameButton.jpg");
+				loadGameButton = Button(281, 60, (screenWidth * 0.5 - screenWidth * 0.35), (screenHeight * 0.6), (screenWidth * 0.7), (screenHeight * 0.2), "../LeytonFinalProjectC++/Sprites/TitleScreenSprites/LoadGameButton.jpg");
 				background = al_load_bitmap("../LeytonFinalProjectC++/Sprites/TitleScreenSprites/BackgroundTitle.jpg");
 				hasLoadedButtons = true;
 
@@ -64,10 +66,10 @@ public:
 			}
 
 			al_draw_scaled_bitmap(background, 0, 0, 728, 410, 0, 0, screenWidth, screenHeight, 0);
-			al_draw_scaled_bitmap(newGameButton, 0, 0, 281, 60, screenWidth * 0.5 - screenWidth * 0.35, screenHeight * 0.3, screenWidth * 0.7,  screenHeight * 0.2, 0);
-			al_draw_scaled_bitmap(loadGameButton, 0, 0, 281, 60, screenWidth * 0.5 - screenWidth * 0.35, screenHeight * 0.6, screenWidth * 0.7, screenHeight * 0.2, 0);
-
-
+			newGameButton.drawSprite();
+			loadGameButton.drawSprite();
+			//al_draw_scaled_bitmap(newGameButton.spriteImage, 0, 0, 281, 60, screenWidth * 0.5 - screenWidth * 0.35, screenHeight * 0.3, screenWidth * 0.7,  screenHeight * 0.2, 0);
+			//al_draw_scaled_bitmap(loadGameButton.spriteImage, 0, 0, 281, 60, screenWidth * 0.5 - screenWidth * 0.35, screenHeight * 0.6, screenWidth * 0.7, screenHeight * 0.2, 0);
 		}
 		
 	}
