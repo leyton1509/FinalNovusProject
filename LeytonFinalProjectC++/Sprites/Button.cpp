92d49b20fb5b11c3cd5290582ac6848081725476
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-17 12:07:52 +0000

    Updated the button class to have an empty constructor, as c++ requires it when declaring, added the code to draw the button too

--- LeytonFinalProjectC++/Sprites/Button.cpp
+++ LeytonFinalProjectC++/Sprites/Button.cpp
@@ -4,12 +4,21 @@ class Button : public Sprite {
 
 public:
 
-	Button(float _xStart, float _yStart, float _width, float _height, const char * _filepath) : Sprite(SpriteType::SpriteTypes::Button, _xStart, _yStart, _width, _height, _filepath) {
+	int originalSizeX;
+	int originalSizeY;
+
+	Button(int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height, const char * _filepath) : Sprite(SpriteType::SpriteTypes::Button, _xStart, _yStart, _width, _height, _filepath) {
+		originalSizeX = _originalSizeX;
+		originalSizeY = _originalSizeY;
 	}
 
+	Button() : Sprite(SpriteType::SpriteTypes::Button, 0,0,0,0, "../LeytonFinalProjectC++/Sprites/TitleScreenSprites/NewGameButton.jpg") {
+		originalSizeX = 0;
+		originalSizeY = 0;
+	}
 
 	void drawSprite() {
-		
+		al_draw_scaled_bitmap(spriteImage, 0, 0, originalSizeX, originalSizeY, xPosition, yPosition, spritewidth, spriteHeight, 0);
 	}
 
 	bool hasBeenClicked(float xMousePosition, float yMousePosition) {
