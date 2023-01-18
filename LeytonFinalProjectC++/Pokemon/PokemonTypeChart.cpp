482add123aff26eff96ff3fb329a711b008b696d
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-18 12:55:06 +0000

    Added a pokemon type chart class which will work out the effectiveness of a move based on the type of pokemon its being used against

---
+++ LeytonFinalProjectC++/Pokemon/PokemonTypeChart.cpp
@@ -0,0 +1,57 @@
+#include "../Pokemon/PokemonType.h"
+
+
+class PokemonTypeChart {
+
+public:
+
+	int returnEffectiveNessOfMove(PokemonType::PokemonTypes typeOfMove, PokemonType::PokemonTypes typeOfPokemon) {
+
+		int effectiveness = 1;
+
+		switch (typeOfMove)
+		{
+		case PokemonType::Normal:
+			break;
+		case PokemonType::Fire:
+			break;
+		case PokemonType::Water:
+			break;
+		case PokemonType::Grass:
+			break;
+		case PokemonType::Electric:
+			break;
+		case PokemonType::Ice:
+			break;
+		case PokemonType::Fighting:
+			break;
+		case PokemonType::Poison:
+			break;
+		case PokemonType::Ground:
+			break;
+		case PokemonType::Flying:
+			break;
+		case PokemonType::Psychic:
+			break;
+		case PokemonType::Bug:
+			break;
+		case PokemonType::Rock:
+			break;
+		case PokemonType::Ghost:
+			break;
+		case PokemonType::Dark:
+			break;
+		case PokemonType::Dragon:
+			break;
+		case PokemonType::Steel:
+			break;
+		case PokemonType::Fairy:
+			break;
+		default:
+			break;
+		}
+
+		return effectiveness;
+	}
+
+};
