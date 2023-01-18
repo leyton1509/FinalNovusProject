774fee308f3fd95209ff055c0b7139d81d94529c
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-18 14:15:17 +0000

    Added electric effectiveness

--- LeytonFinalProjectC++/Pokemon/PokemonTypeChart.cpp
+++ LeytonFinalProjectC++/Pokemon/PokemonTypeChart.cpp
@@ -258,8 +258,68 @@ public:
 			}
 			break;
 		case PokemonType::Electric:
+			switch (typeOfPokemon)
+			{
+			case PokemonType::Normal:
+				effectiveness = effectiveness * 1;
+				break;
+			case PokemonType::Fire:
+				effectiveness = effectiveness * 1;
+				break;
+			case PokemonType::Water:
+				effectiveness = effectiveness * 2;
+				break;
+			case PokemonType::Grass:
+				effectiveness = effectiveness * 0.5;
+				break;
+			case PokemonType::Electric:
+				effectiveness = effectiveness * 0.5;
+				break;
+			case PokemonType::Ice:
+				effectiveness = effectiveness * 1;
+				break;
+			case PokemonType::Fighting:
+				effectiveness = effectiveness * 1;
+				break;
+			case PokemonType::Poison:
+				effectiveness = effectiveness * 1;
+				break;
+			case PokemonType::Ground:
+				effectiveness = effectiveness * 0;
+				break;
+			case PokemonType::Flying:
+				effectiveness = effectiveness * 2;
+				break;
+			case PokemonType::Psychic:
+				effectiveness = effectiveness * 1;
+				break;
+			case PokemonType::Bug:
+				effectiveness = effectiveness * 1;
+				break;
+			case PokemonType::Rock:
+				effectiveness = effectiveness * 1;
+				break;
+			case PokemonType::Ghost:
+				effectiveness = effectiveness * 1;
+				break;
+			case PokemonType::Dark:
+				effectiveness = effectiveness * 1;
+				break;
+			case PokemonType::Dragon:
+				effectiveness = effectiveness * 0.5;
+				break;
+			case PokemonType::Steel:
+				effectiveness = effectiveness * 1;
+				break;
+			case PokemonType::Fairy:
+				effectiveness = effectiveness * 1;
+				break;
+			default:
+				break;
+			}
 			break;
 		case PokemonType::Ice:
+
 			break;
 		case PokemonType::Fighting:
 			break;
