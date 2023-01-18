ac98609c752bc4b5ac9d6aa42509268890b05caa
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-18 16:45:43 +0000

    Added constructor for a move

--- LeytonFinalProjectC++/Pokemon/Moves/Move.cpp
+++ LeytonFinalProjectC++/Pokemon/Moves/Move.cpp
@@ -13,6 +13,15 @@ public:
 	int power;
 	int accuracy;
 
+	Move(int _moveID, const char* _moveName, PokemonType::PokemonTypes _pokemonTypeOfMove, MoveCategory::Categories _typeOfMove, int _powerpoints, int _power, int _accuracy) {
+		moveID = _moveID;
+		moveName = _moveName;
+		pokemonTypeOfMove = _pokemonTypeOfMove;
+		typeOfMove = _typeOfMove;
+		powerpoints = _powerpoints;
+		power = _power;
+		accuracy = _accuracy;
+	}
 
 
 };
