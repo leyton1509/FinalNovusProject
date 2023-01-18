4c5ccdec85195f48db2a57406b45ac6585abae85
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-18 16:40:34 +0000

    Fixed move as it is in different folder

---
+++ LeytonFinalProjectC++/Pokemon/Moves/Move.cpp
@@ -0,0 +1,17 @@
+#include "../../Pokemon/PokemonType.h"
+
+class Move {
+
+public:
+
+	int moveID;
+	const char* moveName;
+	PokemonType::PokemonTypes typeOfMove;
+
+	int powerpoints;
+	int power;
+	int accuracy;
+
+
+
+};
