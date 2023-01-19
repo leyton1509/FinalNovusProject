118166925fac87aff2824e8d222da0918f1494d1
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-19 18:43:43 +0000

    Added a method to convert string of pokemon type to enum

--- LeytonFinalProjectC++/Pokemon/Moves/MoveManager.cpp
+++ LeytonFinalProjectC++/Pokemon/Moves/MoveManager.cpp
@@ -25,13 +25,75 @@ public:
         }
     }
 
+    PokemonType::PokemonTypes getEnumFromString(string typeString) {
+        if (typeString.compare("Normal") == 0) {
+
+        }
+        else if (typeString.compare("Fire") == 0) {
+            return PokemonType::PokemonTypes::Fire;
+        }
+        else if (typeString.compare("Water") == 0) {
+            return PokemonType::PokemonTypes::Water;
+        }
+        else if (typeString.compare("Grass") == 0) {
+            return PokemonType::PokemonTypes::Grass;
+        }
+        else if (typeString.compare("Electric") == 0) {
+            return PokemonType::PokemonTypes::Electric;
+        }
+        else if (typeString.compare("Ice") == 0) {
+            return PokemonType::PokemonTypes::Ice;
+        }
+        else if (typeString.compare("Poison") == 0) {
+            return PokemonType::PokemonTypes::Poison;
+        }
+        else if (typeString.compare("Ground") == 0) {
+            return PokemonType::PokemonTypes::Ground;
+        }
+        else if (typeString.compare("Flying") == 0) {
+            return PokemonType::PokemonTypes::Flying;
+        }
+        else if (typeString.compare("Psychic") == 0) {
+            return PokemonType::PokemonTypes::Psychic;
+        }
+        else if (typeString.compare("Bug") == 0) {
+            return PokemonType::PokemonTypes::Bug;
+        }
+        else if (typeString.compare("Rock") == 0) {
+            return PokemonType::PokemonTypes::Rock;
+        }
+        else if (typeString.compare("Ghost") == 0) {
+            return PokemonType::PokemonTypes::Ghost;
+        }
+        else if (typeString.compare("Dark") == 0) {
+            return PokemonType::PokemonTypes::Dark;
+        }
+        else if (typeString.compare("Dragon") == 0) {
+            return PokemonType::PokemonTypes::Dragon;
+        }
+        else if (typeString.compare("Steel") == 0) {
+            return PokemonType::PokemonTypes::Steel;
+        }
+        else if (typeString.compare("Fairy") == 0) {
+            return PokemonType::PokemonTypes::Fairy;
+        }
+        else {
+            return PokemonType::PokemonTypes::None;
+        }
+    }
+
     MoveManager() {
 
         CSVFileManager csvm = CSVFileManager();
 
-        std::vector<std::vector<std::string> > cs = csvm.parseCSV("../../Pokemon/Moves/PokemonMoves.csv");
-        cout << cs.size() << "\n";
+        std::vector<std::vector<std::string> > cs = csvm.parseCSV("C:/Users/leyto/source/repos/LeytonFinalProjectC++/LeytonFinalProjectC++/Pokemon/Moves/PokemonMoves.csv");
+        
         for (int i = 0; i < cs.size(); i++) {
+
+            int moveID = std::stoi(cs.at(i)[0]);
+            string moveName = cs.at(i)[1];
+            PokemonType::PokemonTypes type = getEnumFromString(cs.at(i)[2]);
+
             cout << cs.at(i)[2] << "\n";
         }
 
