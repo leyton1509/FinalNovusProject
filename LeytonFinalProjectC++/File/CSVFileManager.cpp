37c6bfa43ec9084ba8b5a79893d3b160028dda80
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-19 18:23:27 +0000

    Added the correct reader from main

--- LeytonFinalProjectC++/File/CSVFileManager.cpp
+++ LeytonFinalProjectC++/File/CSVFileManager.cpp
@@ -1,6 +1,32 @@
+#include <fstream>
+#include <vector>
+#include <sstream> 
+
 class CSVFileManager {
 
 	public:
+
+
+
+        std::vector<std::vector<std::string> > parseCSV()
+        {
+            std::ifstream  data("c:/Users/leyto/Downloads/Test.csv");
+            std::string line;
+            std::vector<std::vector<std::string> > parsedCsv;
+            while (std::getline(data, line))
+            {
+                std::stringstream lineStream(line);
+                std::string cell;
+                std::vector<std::string> parsedRow;
+                while (std::getline(lineStream, cell, ','))
+                {
+                    parsedRow.push_back(cell);
+                }
+
+                parsedCsv.push_back(parsedRow);
+            }
+            return parsedCsv;
+        };
 
 
 };
