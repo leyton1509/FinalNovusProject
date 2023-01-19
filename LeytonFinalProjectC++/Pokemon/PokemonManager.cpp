bd1891b80e5e4889bd68244ccd29fca9d6274c02
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-19 21:27:01 +0000

    Added the csv file manager to pokemon, need to create pokemon csv for it

--- LeytonFinalProjectC++/Pokemon/PokemonManager.cpp
+++ LeytonFinalProjectC++/Pokemon/PokemonManager.cpp
@@ -25,6 +25,11 @@ public:
     }
 
     PokemonManager() {
+        CSVFileManager csvm = CSVFileManager();
+
+        std::vector<std::vector<std::string> > cs = csvm.parseCSV("C:/Users/leyto/source/repos/LeytonFinalProjectC++/LeytonFinalProjectC++/Pokemon/Pokemon.csv");
+
+
         map<int, int >levelUpMoveSet = { {1,1}, {2,7} };
         defaultPokemonStorage.insert({ "Bulbasaur", Pokemon("Charizard", 100, 110, 70, 140, 80, 100, 1, 0,0,PokemonType::PokemonTypes::Fire, PokemonType::PokemonTypes::Flying, levelUpMoveSet) });
     }
