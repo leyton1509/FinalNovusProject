650b1695c3e257562c756ec82d231d1de3d56b81
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-20 10:14:27 +0000

    Removed the movetype method into pokemon type

--- LeytonFinalProjectC++/Pokemon/Moves/MoveManager.cpp
+++ LeytonFinalProjectC++/Pokemon/Moves/MoveManager.cpp
@@ -25,62 +25,7 @@ public:
         }
     }
 
-    PokemonType::PokemonTypes getEnumFromStringMove(string typeString) {
-        if (typeString.compare("Normal") == 0) {
-
-        }
-        else if (typeString.compare("Fire") == 0) {
-            return PokemonType::PokemonTypes::Fire;
-        }
-        else if (typeString.compare("Water") == 0) {
-            return PokemonType::PokemonTypes::Water;
-        }
-        else if (typeString.compare("Grass") == 0) {
-            return PokemonType::PokemonTypes::Grass;
-        }
-        else if (typeString.compare("Electric") == 0) {
-            return PokemonType::PokemonTypes::Electric;
-        }
-        else if (typeString.compare("Ice") == 0) {
-            return PokemonType::PokemonTypes::Ice;
-        }
-        else if (typeString.compare("Poison") == 0) {
-            return PokemonType::PokemonTypes::Poison;
-        }
-        else if (typeString.compare("Ground") == 0) {
-            return PokemonType::PokemonTypes::Ground;
-        }
-        else if (typeString.compare("Flying") == 0) {
-            return PokemonType::PokemonTypes::Flying;
-        }
-        else if (typeString.compare("Psychic") == 0) {
-            return PokemonType::PokemonTypes::Psychic;
-        }
-        else if (typeString.compare("Bug") == 0) {
-            return PokemonType::PokemonTypes::Bug;
-        }
-        else if (typeString.compare("Rock") == 0) {
-            return PokemonType::PokemonTypes::Rock;
-        }
-        else if (typeString.compare("Ghost") == 0) {
-            return PokemonType::PokemonTypes::Ghost;
-        }
-        else if (typeString.compare("Dark") == 0) {
-            return PokemonType::PokemonTypes::Dark;
-        }
-        else if (typeString.compare("Dragon") == 0) {
-            return PokemonType::PokemonTypes::Dragon;
-        }
-        else if (typeString.compare("Steel") == 0) {
-            return PokemonType::PokemonTypes::Steel;
-        }
-        else if (typeString.compare("Fairy") == 0) {
-            return PokemonType::PokemonTypes::Fairy;
-        }
-        else {
-            return PokemonType::PokemonTypes::None;
-        }
-    }
+   
 
     MoveCategory::Categories getEnumFromStringCategory(string moveString) {
         if (moveString.compare("Physical") == 0) {
@@ -102,9 +47,11 @@ public:
         
         for (int i = 0; i < cs.size(); i++) {
 
+            PokemonType pt = PokemonType();
+
             int moveID = std::stoi(cs.at(i)[0]);
             string moveName = cs.at(i)[1];
-            PokemonType::PokemonTypes type = getEnumFromStringMove(cs.at(i)[2]);
+            PokemonType::PokemonTypes type = pt.getEnumFromStringMove(cs.at(i)[2]);
             MoveCategory::Categories moveC = getEnumFromStringCategory(cs.at(i)[3]);
             int powerpoints = std::stoi(cs.at(i)[4]);
             int power = std::stoi(cs.at(i)[5]);
