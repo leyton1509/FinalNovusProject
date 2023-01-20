fa9690ad308509c4b5fb467da5dc6f9059a48e42
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-20 16:32:11 +0000

    Started adding logic for the main loop

--- LeytonFinalProjectC++/Battle/InitiateBattle.cpp
+++ LeytonFinalProjectC++/Battle/InitiateBattle.cpp
@@ -4,10 +4,29 @@ class InitiateBattle {
 
 public:
 
-	InitiateBattle(PlayerCharacter player, int locationNumber, int battleType) {
+	InitiateBattle(ALLEGRO_EVENT_QUEUE* queue ,PlayerCharacter player, int locationNumber, int battleType) {
+
+		bool done = false;
+		bool redraw = true;
+		ALLEGRO_EVENT event;
+
+		bool batteFinished = false;
 
 		if (battleType == 0) {
 			// Wild encounter
+
+			while (!batteFinished) {
+
+				al_wait_for_event(queue, &event);
+
+				switch (event.type)
+				{
+
+				}
+
+			}
+
+			
 
 			
 
