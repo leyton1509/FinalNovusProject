ed5c7f5eb6b78f4f6f7ea2e5a0a2a08d70b5b39a
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-20 15:36:23 +0000

    Move Manager has become a singleton, as I want one single isntance of it with the loaded moves, where I can copy the moves into the pokemon array using the ID of the move

--- LeytonFinalProjectC++/Pokemon/Moves/MoveManager.cpp
+++ LeytonFinalProjectC++/Pokemon/Moves/MoveManager.cpp
@@ -10,7 +10,37 @@ using namespace std;
 class MoveManager
 {
 
+private:
+    MoveManager() {
+        CSVFileManager csvm = CSVFileManager();
+        PokemonType pt = PokemonType();
+        MoveCategory mc = MoveCategory();
+
+        std::vector<std::vector<std::string> > cs = csvm.parseCSV("C:/Users/leyto/source/repos/LeytonFinalProjectC++/LeytonFinalProjectC++/CSVs/PokemonMoves.csv");
+
+        for (int i = 0; i < cs.size(); i++) {
+            int moveID = std::stoi(cs.at(i)[0]);
+            string moveName = cs.at(i)[1];
+            PokemonType::PokemonTypes type = pt.getEnumFromStringMove(cs.at(i)[2]);
+            MoveCategory::Categories moveC = mc.getEnumFromStringCategory(cs.at(i)[3]);
+            int powerpoints = std::stoi(cs.at(i)[4]);
+            int power = std::stoi(cs.at(i)[5]);
+            int accuracy = std::stoi(cs.at(i)[6]);
+            int hasSpecialEffect = std::stoi(cs.at(i)[7]);
+            // cout << moveID << " | " << moveName << " | " << type << " | " << moveC << " | " << powerpoints << " | " << power << " | " << accuracy << " | " <<  "\n";
+            moveStorage.insert({ moveID, Move(moveID,	moveName, type ,moveC,	powerpoints,	power, accuracy, hasSpecialEffect) });
+        }
+
+        //cout << "\n----";
+    }
+
 public:
+
+    static MoveManager& instance()
+    {
+        static MoveManager INSTANCE;
+        return INSTANCE;
+    }
 
     map<int, Move> moveStorage = {};
 
@@ -26,33 +56,5 @@ public:
         }
     }
 
-   
-
-    MoveManager() {
-
-        CSVFileManager csvm = CSVFileManager();
-        PokemonType pt = PokemonType();
-        MoveCategory mc = MoveCategory();
-
-        std::vector<std::vector<std::string> > cs = csvm.parseCSV("C:/Users/leyto/source/repos/LeytonFinalProjectC++/LeytonFinalProjectC++/CSVs/PokemonMoves.csv");
-        
-        for (int i = 0; i < cs.size(); i++) {
-
-            
-
-            int moveID = std::stoi(cs.at(i)[0]);
-            string moveName = cs.at(i)[1];
-            PokemonType::PokemonTypes type = pt.getEnumFromStringMove(cs.at(i)[2]);
-            MoveCategory::Categories moveC = mc.getEnumFromStringCategory(cs.at(i)[3]);
-            int powerpoints = std::stoi(cs.at(i)[4]);
-            int power = std::stoi(cs.at(i)[5]);
-            int accuracy = std::stoi(cs.at(i)[6]);
-            int hasSpecialEffect = std::stoi(cs.at(i)[7]);
-           // cout << moveID << " | " << moveName << " | " << type << " | " << moveC << " | " << powerpoints << " | " << power << " | " << accuracy << " | " <<  "\n";
-            moveStorage.insert({ moveID, Move(moveID,	moveName, type ,moveC,	powerpoints,	power, accuracy, hasSpecialEffect) });
-        }
-
-        //cout << "\n----";
-    }
 
 };
