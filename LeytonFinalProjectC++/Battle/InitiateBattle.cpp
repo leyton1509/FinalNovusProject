ebab70dd99ebc2733766b833b179062d94ef03d2
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-20 16:33:50 +0000

    Fixed spelling and added isplay close and main draw loop

--- LeytonFinalProjectC++/Battle/InitiateBattle.cpp
+++ LeytonFinalProjectC++/Battle/InitiateBattle.cpp
@@ -10,29 +10,37 @@ public:
 		bool redraw = true;
 		ALLEGRO_EVENT event;
 
-		bool batteFinished = false;
+		bool battleFinished = false;
 
 		if (battleType == 0) {
 			// Wild encounter
 
-			while (!batteFinished) {
+			while (!battleFinished) {
 
 				al_wait_for_event(queue, &event);
 
 				switch (event.type)
 				{
+				case ALLEGRO_EVENT_DISPLAY_CLOSE:
+					done = true;
+					break;
+				}
 
+				if (done)
+					break;
+
+				if (redraw && al_is_event_queue_empty(queue))
+				{
+					al_clear_to_color(al_map_rgb(0, 0, 0));
+
+					al_flip_display();
+					redraw = false;
 				}
 
 			}
-
-			
-
-			
-
-
-
 		}
+
+		
 		else if (battleType == 1) {
 			// Pokemon encounter
 
