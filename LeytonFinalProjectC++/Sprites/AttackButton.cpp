419b2127f1be18136a1defd4e8db96a78ff886f2
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-22 13:33:31 +0000

    Correctly inherited button

---
+++ LeytonFinalProjectC++/Sprites/AttackButton.cpp
@@ -0,0 +1,34 @@
+#include "../Sprites/Button.h"
+
+class AttackButton : public Button {
+
+public:
+
+	int originalSizeX;
+	int originalSizeY;
+
+	AttackButton(int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height, const char* _filepath) : Button( _originalSizeX,  _originalSizeY,  _xStart,  _yStart,  _width,  _height,  _filepath) {
+		originalSizeX = _originalSizeX;
+		originalSizeY = _originalSizeY;
+	}
+
+	AttackButton() : Button() {
+		originalSizeX = 0;
+		originalSizeY = 0;
+	}
+
+	void drawSprite() {
+		al_draw_scaled_bitmap(spriteImage, 0, 0, originalSizeX, originalSizeY, xPosition, yPosition, spritewidth, spriteHeight, 0);
+	}
+
+	bool hasBeenClicked(double xMousePosition, double yMousePosition) {
+
+		if (xMousePosition > xPosition && xMousePosition < xPosition + spritewidth && yMousePosition > yPosition && yMousePosition < yPosition + spriteHeight) {
+			return true;
+		}
+		return false;
+	}
+
+
+
+};
