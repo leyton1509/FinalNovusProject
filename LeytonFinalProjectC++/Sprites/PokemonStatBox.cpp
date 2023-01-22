39caf56c6af99f34c41e99d90f58021a515c050b
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-22 11:23:38 +0000

    Added pokemon to the parameters

--- LeytonFinalProjectC++/Sprites/PokemonStatBox.cpp
+++ LeytonFinalProjectC++/Sprites/PokemonStatBox.cpp
@@ -1,19 +1,23 @@
 #include "../Sprites/Spriteheader.h"
+#include "../Pokemon/Pokemon.h"
 class PokemonStatBox : public Sprite {
 
 public:
 
+	Pokemon representedPokemon;
 	int originalSizeX;
 	int originalSizeY;
 
-	PokemonStatBox(int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height, const char* _filepath) : Sprite(SpriteType::SpriteTypes::Button, _xStart, _yStart, _width, _height, _filepath) {
+	PokemonStatBox(Pokemon _pokemonToRepresent, int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height, const char* _filepath) : Sprite(SpriteType::SpriteTypes::Button, _xStart, _yStart, _width, _height, _filepath) {
 		originalSizeX = _originalSizeX;
 		originalSizeY = _originalSizeY;
+		representedPokemon = _pokemonToRepresent;
 	}
 
 	PokemonStatBox() : Sprite(SpriteType::SpriteTypes::PokemonStatBox, 0, 0, 0, 0, "../LeytonFinalProjectC++/Sprites/BattleSprites/PokemonStatBox.png") {
 		originalSizeX = 0;
 		originalSizeY = 0;
+		representedPokemon = Pokemon();
 	}
 
 	void drawSprite() {
