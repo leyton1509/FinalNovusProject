d9f2849ca91e7f528ed669700b8c54d5185d1cc8
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-22 16:08:29 +0000

    Started to work out damage

--- LeytonFinalProjectC++/Battle/PokemonTurn.cpp
+++ LeytonFinalProjectC++/Battle/PokemonTurn.cpp
@@ -1,12 +1,40 @@
 #include "../Pokemon/Pokemon.h"
+#include "../Pokemon/Moves/MoveCategory.h"
 class PokemonTurn {
 	
 public:
 
+	int calcDamageOfMove(Pokemon playersPokemon, Pokemon otherPokemon, Move playerMoveUsed) {
+
+		int attack = 0;
+		int defence = 0;
+
+		if (playerMoveUsed.typeOfMove == MoveCategory::Special) {
+			attack = playersPokemon.specialAttackActual;
+			defence = otherPokemon.specialDefenceActual;
+		}
+
+		int damage = ((2 * playersPokemon.level) + 2) * playerMoveUsed.power * ()
+
+	}
+
 	PokemonTurn(Pokemon& playersPokemon, Pokemon& otherPokemon, Move& playerMoveUsed) 
 	{
-		cout << "Decreasing " << otherPokemon.pokemonName << " health from " << otherPokemon.currentHealth << " to " << (otherPokemon.currentHealth - 5)<< "\n";
-		otherPokemon.decreasePokemonHealth(5); 
+
+		// First of all, check who goes first
+
+		if (playersPokemon.speedActual >= otherPokemon.speedActual) {
+
+
+
+		}
+		else {
+
+		}
+	
+
+
+
 	}
 
 	PokemonTurn(Pokemon& playersPokemon, Pokemon& otherPokemon) {
