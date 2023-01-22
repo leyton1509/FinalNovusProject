09d4ca88d146ea564011c816f6860b3879d5a1b1
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-22 15:55:44 +0000

    Used the & sign to pass by value

--- LeytonFinalProjectC++/Battle/PokemonTurn.cpp
+++ LeytonFinalProjectC++/Battle/PokemonTurn.cpp
@@ -1,13 +1,15 @@
-#include "../Pokemon/Pokemon.cpp"
+#include "../Pokemon/Pokemon.h"
 class PokemonTurn {
 	
 public:
 
-	PokemonTurn(Pokemon playersPokemon, Pokemon otherPokemon, Move playerMoveUsed) {
-
+	PokemonTurn(Pokemon& playersPokemon, Pokemon& otherPokemon, Move& playerMoveUsed) 
+	{
+		cout << "Decreasing " << otherPokemon.pokemonName << " health from " << otherPokemon.currentHealth << " to " << (otherPokemon.currentHealth - 5)<< "\n";
+		otherPokemon.decreasePokemonHealth(5); 
 	}
 
-	PokemonTurn(Pokemon playersPokemon, Pokemon otherPokemon) {
+	PokemonTurn(Pokemon& playersPokemon, Pokemon& otherPokemon) {
 
 	}
 
