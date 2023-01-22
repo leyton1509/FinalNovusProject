8565d7ba9a52a650d0de4b75066f17ce15a13b6f
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-22 13:35:05 +0000

    Created internal variable pokemon move

--- LeytonFinalProjectC++/Sprites/AttackButton.cpp
+++ LeytonFinalProjectC++/Sprites/AttackButton.cpp
@@ -7,10 +7,12 @@ public:
 
 	int originalSizeX;
 	int originalSizeY;
+	Move pokemonMove;
 
-	AttackButton(Move move, int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height, const char* _filepath) : Button( _originalSizeX,  _originalSizeY,  _xStart,  _yStart,  _width,  _height,  _filepath) {
+	AttackButton(Move _move, int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height, const char* _filepath) : Button( _originalSizeX,  _originalSizeY,  _xStart,  _yStart,  _width,  _height,  _filepath) {
 		originalSizeX = _originalSizeX;
 		originalSizeY = _originalSizeY;
+		pokemonMove = _move;
 	}
 
 	AttackButton() : Button() {
