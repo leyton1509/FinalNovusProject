0b9e48db7fe19ff81abc20cceb391371b1be7b92
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-22 19:24:59 +0000

    Ammended code to handle status conditions

--- LeytonFinalProjectC++/Battle/PokemonTurn.cpp
+++ LeytonFinalProjectC++/Battle/PokemonTurn.cpp
@@ -5,7 +5,7 @@ class PokemonTurn {
 	
 public:
 
-	int calcDamageOfMove(Pokemon playersPokemon, Pokemon otherPokemon, Move playerMoveUsed) {
+	int calcDamageOfMove(Pokemon attackingPokemon, Pokemon DefendingPokemon, Move playerMoveUsed) {
 
 
 
@@ -13,32 +13,37 @@ public:
 		int defence = 0;
 
 		if (playerMoveUsed.typeOfMove == MoveCategory::Special) {
-			attack = playersPokemon.specialAttackActual;
-			defence = otherPokemon.specialDefenceActual;
+			attack = attackingPokemon.specialAttackActual;
+			defence = DefendingPokemon.specialDefenceActual;
 		}
 		else if (playerMoveUsed.typeOfMove == MoveCategory::Physical) {
-			attack = playersPokemon.physcialAttackActual;
-			defence = otherPokemon.physicalDefenceActual;
+			attack = attackingPokemon.physcialAttackActual;
+			defence = DefendingPokemon.physicalDefenceActual;
 		}
 
 		PokemonTypeChart ptc = PokemonTypeChart();
-		int effectivenessOfMove = ptc.returnEffectivenessOfMove(playerMoveUsed.pokemonTypeOfMove, otherPokemon.pokemonTypeOne, otherPokemon.pokemonTypeTwo);
+		int effectivenessOfMove = ptc.returnEffectivenessOfMove(playerMoveUsed.pokemonTypeOfMove, DefendingPokemon.pokemonTypeOne, DefendingPokemon.pokemonTypeTwo);
 
 		int stab = 1;
 
-		if (playersPokemon.pokemonTypeOne == playerMoveUsed.pokemonTypeOfMove || playersPokemon.pokemonTypeTwo == playerMoveUsed.pokemonTypeOfMove) {
+		if (attackingPokemon.pokemonTypeOne == playerMoveUsed.pokemonTypeOfMove || attackingPokemon.pokemonTypeTwo == playerMoveUsed.pokemonTypeOfMove) {
 			stab = 1.5;
 		}
-		int ranModifier = ((rand()%85) + 100);
-		int damage = (((2 * playersPokemon.level) + 2) * playerMoveUsed.power * (attack / defence) / 50) * effectivenessOfMove * stab * ranModifier;
+		//int ranModifier = ((rand()%85) + 100);
+		int damage = (((2 * attackingPokemon.level) + 2) * playerMoveUsed.power * (attack / defence) / 50) * effectivenessOfMove * stab;
+		cout << "\nPokemon Attacking : |"<< attackingPokemon.pokemonName << " Damage:" << damage << " Effectiveness:" << effectivenessOfMove << "\n";
 		return damage;
+	}
+
+	int random(int from, int to) {
+		return rand() % (to - from + 1) + from;
 	}
 
 	Move getEnemyMoveUsed(Pokemon playersPokemon, Pokemon otherPokemon, int aiLevel) {
 
 		if (aiLevel == 0) {
 			int numberOfMoves = otherPokemon.numberOfMoves-1;
-			int ran = ((rand()) + numberOfMoves-1);
+			int ran = random(0, (numberOfMoves-1));
 			return otherPokemon.pokemonsMoves[ran];
 		}
 		else if (aiLevel == 1) {
@@ -59,16 +64,35 @@ public:
 
 	}
 
+	void statusMove(Pokemon& pokemonUsingMove, Pokemon& otherPokemon, Move statusMoveUsed) {
+
+	}
+
 	void enemyAttack(Pokemon& playersPokemon, Pokemon& otherPokemon) {
 		if (otherPokemon.level > 15) {
 			Move enemyMove = getEnemyMoveUsed(playersPokemon, otherPokemon, 1);
-			int damageOfEnemyMove = calcDamageOfMove(otherPokemon, playersPokemon, enemyMove);
-			playersPokemon.decreasePokemonHealth(damageOfEnemyMove);
+			if (enemyMove.typeOfMove == MoveCategory::Status) {
+				statusMove(otherPokemon, playersPokemon, enemyMove);
+			}
+			else {
+				int damageOfEnemyMove = calcDamageOfMove(otherPokemon, playersPokemon, enemyMove);
+				playersPokemon.decreasePokemonHealth(damageOfEnemyMove);
+			}
+			
 		}
 		else {
 			Move enemyMove = getEnemyMoveUsed(playersPokemon, otherPokemon, 0);
-			int damageOfEnemyMove = calcDamageOfMove(otherPokemon, playersPokemon, enemyMove);
-			playersPokemon.decreasePokemonHealth(damageOfEnemyMove);
+			if (enemyMove.typeOfMove == MoveCategory::Status) {
+				statusMove(otherPokemon, playersPokemon, enemyMove);
+			}
+			else {
+				cout << "\nUsed : " << enemyMove.moveName << "\n";
+				int damageOfEnemyMove = calcDamageOfMove(otherPokemon, playersPokemon, enemyMove);
+				playersPokemon.decreasePokemonHealth(damageOfEnemyMove);
+				cout << "\Player health : " << playersPokemon.currentHealth << " taken " << damageOfEnemyMove << "\n";
+			}
+
+			
 		}
 	}
 
@@ -76,9 +100,18 @@ public:
 	{
 		// First of all, check who goes first
 
+		playersPokemon.printMonsterDetails();
+		otherPokemon.printMonsterDetails();
+
 		if (playersPokemon.speedActual >= otherPokemon.speedActual) {
-			int damageOfMove = calcDamageOfMove(playersPokemon, otherPokemon, playerMoveUsed);
-			otherPokemon.decreasePokemonHealth(damageOfMove);
+			if (playerMoveUsed.typeOfMove == MoveCategory::Status) {
+				statusMove(otherPokemon, playersPokemon, playerMoveUsed);
+			}
+			else {
+				int damageOfMove = calcDamageOfMove(playersPokemon, otherPokemon, playerMoveUsed);
+				otherPokemon.decreasePokemonHealth(damageOfMove);
+			}
+			
 			if (otherPokemon.currentHealth != 0) {
 				enemyAttack(playersPokemon, otherPokemon);
 			}
@@ -87,8 +120,13 @@ public:
 			enemyAttack(playersPokemon, otherPokemon);
 
 			if (playersPokemon.currentHealth != 0) {
-				int damageOfMove = calcDamageOfMove(playersPokemon, otherPokemon, playerMoveUsed);
-				otherPokemon.decreasePokemonHealth(damageOfMove);
+				if (playerMoveUsed.typeOfMove == MoveCategory::Status) {
+					statusMove(otherPokemon, playersPokemon, playerMoveUsed);
+				}
+				else {
+					int damageOfMove = calcDamageOfMove(playersPokemon, otherPokemon, playerMoveUsed);
+					otherPokemon.decreasePokemonHealth(damageOfMove);
+				}
 			}
 		}
 
