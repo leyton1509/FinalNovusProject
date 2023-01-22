1420074d5554f8cc2c14e4c511cf82553b995aa2
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-22 11:38:06 +0000

    Added text to draw pokemon name, need to fix position

--- LeytonFinalProjectC++/Sprites/PokemonStatBox.cpp
+++ LeytonFinalProjectC++/Sprites/PokemonStatBox.cpp
@@ -1,5 +1,7 @@
 #include "../Sprites/Spriteheader.h"
 #include "../Pokemon/Pokemon.h"
+#include <allegro5/allegro_font.h>
+
 class PokemonStatBox : public Sprite {
 
 public:
@@ -7,6 +9,7 @@ public:
 	Pokemon representedPokemon;
 	int originalSizeX;
 	int originalSizeY;
+	ALLEGRO_FONT* font = al_create_builtin_font();
 
 	PokemonStatBox(Pokemon _pokemonToRepresent, int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height, const char* _filepath) : Sprite(SpriteType::SpriteTypes::Button, _xStart, _yStart, _width, _height, _filepath) {
 		originalSizeX = _originalSizeX;
@@ -22,6 +25,7 @@ public:
 
 	void drawSprite() {
 		al_draw_scaled_bitmap(spriteImage, 0, 0, originalSizeX, originalSizeY, xPosition, yPosition, spritewidth, spriteHeight, 0);
+		al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, representedPokemon.pokemonName.c_str());
 	}
 
 
