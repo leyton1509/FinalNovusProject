a758d7b234bb0cdd197aa2bdb2f5a644d4d51b6f
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-23 20:48:03 +0000

    Added a text for textbox variable

--- LeytonFinalProjectC++/Battle/InitiateBattle.cpp
+++ LeytonFinalProjectC++/Battle/InitiateBattle.cpp
@@ -60,7 +60,7 @@ public:
 		Button backBox = Button(256, 128, 5, 435, 445, 165, "../LeytonFinalProjectC++/Sprites/BattleSprites/BackBox.png");
 		Button textBox = Button(256, 128, 500, 470, 345, 165, "../LeytonFinalProjectC++/Sprites/BattleSprites/TextBox.png");
 
-
+		string textForTextBox = "";
 
 		AttackButton attackButton1 = AttackButton(player.trainersParty[currentPokemon].pokemonsMoves[0], 128, 64, 20, 450, 128, 64, "../LeytonFinalProjectC++/Sprites/BattleSprites/AttackButton.png");
 		AttackButton attackButton2 = AttackButton(player.trainersParty[currentPokemon].pokemonsMoves[1],128, 64, 160, 450, 128, 64, "../LeytonFinalProjectC++/Sprites/BattleSprites/AttackButton.png");
