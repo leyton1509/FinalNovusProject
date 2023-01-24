2d6e5f07024ee18c1c2f7f5dcb4599bd30db8678
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-24 09:23:22 +0000

    Removed old x and y as sprite contained this info

--- LeytonFinalProjectC++/Sprites/PokemonStatBox.cpp
+++ LeytonFinalProjectC++/Sprites/PokemonStatBox.cpp
@@ -21,23 +21,20 @@ public:
 	ALLEGRO_BITMAP* whiteHealthBar = al_load_bitmap("../LeytonFinalProjectC++/Sprites/BattleSprites/WhiteHealthBar.png");
 	// THe red health bar that changes width
 	ALLEGRO_BITMAP* redHealthBar = al_load_bitmap("../LeytonFinalProjectC++/Sprites/BattleSprites/RedHealthBar.png");
-	float x;
-	float y;
+
 
 	// The constructor with takes the pokemon to display info for
 	// and the standard size info
 	PokemonStatBox(Pokemon& _pokemonToRepresent, int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height, const char* _filepath) : Sprite(SpriteType::SpriteTypes::Button, _xStart, _yStart, _width, _height, _filepath) {
 		originalSizeX = _originalSizeX;
 		originalSizeY = _originalSizeY;
-		x = _xStart;
-		y = _yStart;
+
 	}
 
 	// Empty Const
 	PokemonStatBox() : Sprite(SpriteType::SpriteTypes::PokemonStatBox, 0, 0, 0, 0, "../LeytonFinalProjectC++/Sprites/BattleSprites/PokemonStatBox.png") {
 		originalSizeX = 0;
 		originalSizeY = 0;
-		x, y = 0;
 	}
 
 
@@ -45,13 +42,13 @@ public:
 	// Draws the back, the health bars and text
 	void drawSprite(Pokemon& representedPokemon) {
 		al_draw_scaled_bitmap(spriteImage, 0, 0, originalSizeX, originalSizeY, xPosition, yPosition, spritewidth, spriteHeight, 0);
-		al_draw_text(font, al_map_rgb(255, 255, 255), (x + 15), (y + 5), 0, (representedPokemon.pokemonName + "   Lvl. " + std::to_string(representedPokemon.level)).c_str());
-		al_draw_scaled_bitmap(whiteHealthBar, 0, 0, 128, 32, x + 15, y + 35, 285, 10, 0);
+		al_draw_text(font, al_map_rgb(255, 255, 255), (xPosition + 15), (yPosition + 5), 0, (representedPokemon.pokemonName + "   Lvl. " + std::to_string(representedPokemon.level)).c_str());
+		al_draw_scaled_bitmap(whiteHealthBar, 0, 0, 128, 32, xPosition + 15, yPosition + 35, 285, 10, 0);
 		//al_draw_filled_rectangle((x + 15), (y + 35), (x + 15 + (static_cast<float>(representedPokemon.currentHealth) / static_cast<float>(representedPokemon.healthActual) * static_cast<float>(285))), (y + 45), al_map_rgba(255, 0, 0, 0.3));
-		al_draw_scaled_bitmap(redHealthBar, 0, 0, 128, 32, x + 15, y + 35, (static_cast<float>(representedPokemon.currentHealth) / static_cast<float>(representedPokemon.healthActual) * static_cast<float>(285)), 10, 0);
+		al_draw_scaled_bitmap(redHealthBar, 0, 0, 128, 32, xPosition + 15, yPosition + 35, (static_cast<float>(representedPokemon.currentHealth) / static_cast<float>(representedPokemon.healthActual) * static_cast<float>(285)), 10, 0);
 
 		// al_draw_scaled_bitmap(spriteImage, 0,0 ,(x + 15), (y + 35), (x + 15 + 285), (y + 45));
-		al_draw_text(fontSmaller, al_map_rgb(255, 255, 255), (x + 15), (y + 50), 0, ("HP: " + std::to_string(representedPokemon.currentHealth) + "/" + std::to_string(representedPokemon.healthActual)).c_str());
+		al_draw_text(fontSmaller, al_map_rgb(255, 255, 255), (xPosition + 15), (yPosition + 50), 0, ("HP: " + std::to_string(representedPokemon.currentHealth) + "/" + std::to_string(representedPokemon.healthActual)).c_str());
 
 	}
 
