63921a531c8aa09bce4fa14cf9df98379ad58ba2
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-24 09:21:52 +0000

    Added comments

--- LeytonFinalProjectC++/Sprites/PokemonStatBox.cpp
+++ LeytonFinalProjectC++/Sprites/PokemonStatBox.cpp
@@ -2,19 +2,30 @@
 #include "../Pokemon/Pokemon.h"
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_primitives.h>
+
+// Class to represent the pokemon stat box
+// Inherits from sprite
 class PokemonStatBox : public Sprite {
 
 public:
 
+	// Original X Size of image
 	int originalSizeX;
+	// Original Y Size of image
 	int originalSizeY;
+	// The font to display with
 	ALLEGRO_FONT* font = al_load_font("MagzoSemiBold-GOraO.otf", 28, NULL);
+	// A smaller font to display with
 	ALLEGRO_FONT* fontSmaller = al_load_font("MagzoSemiBold-GOraO.otf", 16, NULL);
+	// The white health bar
 	ALLEGRO_BITMAP* whiteHealthBar = al_load_bitmap("../LeytonFinalProjectC++/Sprites/BattleSprites/WhiteHealthBar.png");
+	// THe red health bar that changes width
 	ALLEGRO_BITMAP* redHealthBar = al_load_bitmap("../LeytonFinalProjectC++/Sprites/BattleSprites/RedHealthBar.png");
 	float x;
 	float y;
 
+	// The constructor with takes the pokemon to display info for
+	// and the standard size info
 	PokemonStatBox(Pokemon& _pokemonToRepresent, int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height, const char* _filepath) : Sprite(SpriteType::SpriteTypes::Button, _xStart, _yStart, _width, _height, _filepath) {
 		originalSizeX = _originalSizeX;
 		originalSizeY = _originalSizeY;
@@ -22,12 +33,16 @@ public:
 		y = _yStart;
 	}
 
+	// Empty Const
 	PokemonStatBox() : Sprite(SpriteType::SpriteTypes::PokemonStatBox, 0, 0, 0, 0, "../LeytonFinalProjectC++/Sprites/BattleSprites/PokemonStatBox.png") {
 		originalSizeX = 0;
 		originalSizeY = 0;
 		x, y = 0;
 	}
 
+
+	// Draw sprite overrides sprite
+	// Draws the back, the health bars and text
 	void drawSprite(Pokemon& representedPokemon) {
 		al_draw_scaled_bitmap(spriteImage, 0, 0, originalSizeX, originalSizeY, xPosition, yPosition, spritewidth, spriteHeight, 0);
 		al_draw_text(font, al_map_rgb(255, 255, 255), (x + 15), (y + 5), 0, (representedPokemon.pokemonName + "   Lvl. " + std::to_string(representedPokemon.level)).c_str());
@@ -40,6 +55,7 @@ public:
 
 	}
 
+	// Destroys all the sprites 
 	void destroySprites() {
 		al_destroy_bitmap(spriteImage);
 		al_destroy_font(font);
