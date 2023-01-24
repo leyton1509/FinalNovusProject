6dfcb05a93963c320e50eaf6d4a2f11e912f086f
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-24 09:25:19 +0000

    Added comments

--- LeytonFinalProjectC++/Sprites/PokemonGrass.cpp
+++ LeytonFinalProjectC++/Sprites/PokemonGrass.cpp
@@ -1,20 +1,23 @@
 #include "../Sprites/Spriteheader.h"
 
+// Class to represent pokemon grass
+// May not be needed yet
+// Inherits sprite info
 class PokemonGrass : public Sprite {
 
 public:
 
+	// Constructor to place the sprite at
 	PokemonGrass(float _startXPosition, float _startYPosition) : Sprite(SpriteType::SpriteTypes::Grass, _startXPosition, _startYPosition, 32, 32, "../LeytonFinalProjectC++/Sprites/MapSprites/PokemonGrass.png") {
 	}
 
+	// Empty Const
 	PokemonGrass() : Sprite(SpriteType::SpriteTypes::Grass, 0, 0, 32, 32, "../LeytonFinalProjectC++/Sprites/MapSprites/PokemonGrass.png") {
 	}
 
-
+	// Draws the sprite at x and y
 	void drawSprite() {
 		al_draw_bitmap(spriteImage, xPosition, yPosition, 0);
 	}
-
-
 
 };
