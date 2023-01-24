46c3da405544a4914cc5e49dcd9862d665dcc9fd
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-24 09:18:19 +0000

    Added comments

--- LeytonFinalProjectC++/Sprites/SpriteType.cpp
+++ LeytonFinalProjectC++/Sprites/SpriteType.cpp
@@ -1,7 +1,10 @@
+
+// Class to define the different sprite types
 class SpriteType
 {
 public:
 
+	// Each sprite option
 	enum SpriteTypes
 	{
 		Player, Pokemon, Building, Grass, Entrance, Button, PokemonStatBox
