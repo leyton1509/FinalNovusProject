8488a661e351e09f4f5027dbf6f15b8ddfaa3c3f
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-25 20:08:23 +0000

    Fixed headers that were crashing code

--- LeytonFinalProjectC++/Sprites/InteractablesForMaps.cpp
+++ LeytonFinalProjectC++/Sprites/InteractablesForMaps.cpp
@@ -1,8 +1,20 @@
+
+#include <list>
+#include "../Sprites/Interactable.h"
+#include "../Sprites/Breifcase.h"
 class InteractablesForMaps {
 
 public:
 
-	InteractablesForMaps() {
+	std::list<Interactable> getInteractablesForMap(int mapNumber) {
+		std::list<Interactable> interactbles;
+
+		if (mapNumber == 1) {
+			//Breifcase bf = Breifcase(220, 165, );
+		}
+
+		return  interactbles;
+
 
 	}
 
