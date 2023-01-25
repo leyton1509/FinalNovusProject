6e2741ea2ba3fd59c3213b1cddecc8eeab54069c
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-25 19:35:56 +0000

    Change disapeear to true

--- LeytonFinalProjectC++/Sprites/Breifcase.cpp
+++ LeytonFinalProjectC++/Sprites/Breifcase.cpp
@@ -4,9 +4,8 @@ class Breifcase : public Interactable {
 
 public:
 
-	Breifcase(bool _dissapeaarWhenInteractedWith, int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height, const char* _filepath) : Interactable(_dissapeaarWhenInteractedWith, _originalSizeX, _originalSizeY, _xStart, _yStart, _width, _height, _filepath) {
+	Breifcase(int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height, const char* _filepath) : Interactable(true, _originalSizeX, _originalSizeY, _xStart, _yStart, _width, _height, _filepath) {
 		originalSizeX = _originalSizeX;
 		originalSizeY = _originalSizeY;
-		dissapeaarWhenInteractedWith = _dissapeaarWhenInteractedWith;
 	}
 };
