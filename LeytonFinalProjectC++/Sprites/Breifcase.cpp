c28c426690225317fb2ef4b936562a588409585e
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-26 09:41:01 +0000

    Treid printing out the interactavble, however c++ doesnt allow downcasting for these types, so have to create interactbales of the different types inside this to sore it as a list

--- LeytonFinalProjectC++/Sprites/Breifcase.cpp
+++ LeytonFinalProjectC++/Sprites/Breifcase.cpp
@@ -4,12 +4,12 @@ class Breifcase : public Interactable {
 
 public:
 
-	Breifcase(int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height, const char* _filepath) : Interactable(true, _originalSizeX, _originalSizeY, _xStart, _yStart, _width, _height, _filepath) {
+	Breifcase(int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height, const char* _filepath) : Interactable(1, true, _originalSizeX, _originalSizeY, _xStart, _yStart, _width, _height, _filepath) {
 		originalSizeX = _originalSizeX;
 		originalSizeY = _originalSizeY;
 	}
 
-	void interact() {
-
+	void interact() override {
+		std::cout << "Interacting!";
 	}
 };
