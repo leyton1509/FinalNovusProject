3ad7acb9b84eb6bb72997f3e110fe99793881ade
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-26 14:44:55 +0000

    Changed position of case

--- LeytonFinalProjectC++/Sprites/InteractablesForMaps.cpp
+++ LeytonFinalProjectC++/Sprites/InteractablesForMaps.cpp
@@ -9,7 +9,7 @@ public:
 		std::list<Interactable> interactbles;
 
 		if (mapNumber == 1) {
-			Interactable bf = Interactable(1, true, 220, 165, 14 * 32, 21 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/MapSprites/Case.png");
+			Interactable bf = Interactable(1, true, 220, 165, 15 * 32, 24 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/MapSprites/Case.png");
 			interactbles.push_front(bf);
 
 			return interactbles;
