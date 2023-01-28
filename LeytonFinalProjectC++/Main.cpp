e6f3e4fc1dad29f5b24686b40622824a350dbe2a
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-28 11:59:51 +0000

    brought player down the map for testing

--- LeytonFinalProjectC++/Main.cpp
+++ LeytonFinalProjectC++/Main.cpp
@@ -232,7 +232,7 @@ int main()
     player.trainersParty[0].setPokemonsLevel(7);
     //player.xPosition = screenWidth / 2;
     //player.yPosition = screenHeight / 2;
-    player.setAllPositions(15, 10);
+    player.setAllPositions(15, 20);
     WorldMap worldMap = WorldMap(screenWidth, screenHeight, "../LeytonFinalProjectC++/WorldMap/TextMaps/MapOne.txt", 1);
     float cameraPosition[2] = { 0,0 };
 
