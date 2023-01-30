fdaf89a50334cd5cb3b6b0d517ec5eada05c373b
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-30 11:03:39 +0000

    Removed switch for button 4

--- LeytonFinalProjectC++/Battle/InitiateBattle.cpp
+++ LeytonFinalProjectC++/Battle/InitiateBattle.cpp
@@ -205,18 +205,6 @@ public:
 
 					else if (switchPokemonFourButton.hasBeenClicked(xMousePosition, yMousePosition)) {
 						switchPokemonButtonClicked = 4;
-						if (!(strcmp(player.trainersParty[3].pokemonName.c_str(), "") == 0)) {
-							if (currentPokemon != 3) {
-								if (player.trainersParty[3].currentHealth != 0) {
-									currentPokemon = 3;
-									attackButton1.pokemonMove = player.trainersParty[currentPokemon].pokemonsMoves[0];
-									attackButton2.pokemonMove = player.trainersParty[currentPokemon].pokemonsMoves[1];
-									attackButton3.pokemonMove = player.trainersParty[currentPokemon].pokemonsMoves[2];
-									attackButton4.pokemonMove = player.trainersParty[currentPokemon].pokemonsMoves[3];
-									PokemonTurn doTurn = PokemonTurn(player.trainersParty[currentPokemon], otherPokemon);
-								}
-							}
-						}
 					}
 
 					else if (switchPokemonFiveButton.hasBeenClicked(xMousePosition, yMousePosition)) {
