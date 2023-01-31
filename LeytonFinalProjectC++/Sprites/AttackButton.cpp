5ed0929cf014c371b691533043949b3c05d3ff95
taylor92 <taylor92@lancaster.ac.uk>
2023-01-31 21:24:58 +0000

    Moved fixed file path into this class

--- LeytonFinalProjectC++/Sprites/AttackButton.cpp
+++ LeytonFinalProjectC++/Sprites/AttackButton.cpp
@@ -18,7 +18,7 @@ public:
 	Move pokemonMove;
 
 	// Connstructor with the move to be used and the positioning parameters
-	AttackButton(Move& _move, int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height, const char* _filepath) : Button( _originalSizeX,  _originalSizeY,  _xStart,  _yStart,  _width,  _height,  _filepath) {
+	AttackButton(Move& _move, int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height) : Button( _originalSizeX,  _originalSizeY,  _xStart,  _yStart,  _width,  _height, "../LeytonFinalProjectC++/Sprites/BattleSprites/AttackButton.png") {
 		originalSizeX = _originalSizeX;
 		originalSizeY = _originalSizeY;
 		pokemonMove = _move;
