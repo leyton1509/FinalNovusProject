548b2e0ec47288df396f3755e771cf8f68eb7735
taylor92 <taylor92@lancaster.ac.uk>
2023-01-31 16:15:53 +0000

    added integers for the number of each pokeball and potions

--- LeytonFinalProjectC++/Sprites/Items/ItemManager.cpp
+++ LeytonFinalProjectC++/Sprites/Items/ItemManager.cpp
@@ -2,6 +2,14 @@ class ItemManager {
 
 public:
 
+	int numberOfPotions;
+	int numberOfSuperPotions;
+	int numberOfHyperPotions;
+
+	int numberOfPokeBalls;
+	int numberOfGreatPokeBalls;
+	int numberOfUltraPokeBalls;
+
 	ItemManager() {
 
 	}
