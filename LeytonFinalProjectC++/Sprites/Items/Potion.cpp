53e71a1a4e2f09d1fe404f2d3cdc20e8c43231d3
taylor92 <taylor92@lancaster.ac.uk>
2023-01-31 15:35:57 +0000

    Added switch to heal pokemon based on the potion type

--- LeytonFinalProjectC++/Sprites/Items/Potion.cpp
+++ LeytonFinalProjectC++/Sprites/Items/Potion.cpp
@@ -12,6 +12,19 @@ public:
 	Potion(string _potionName): Item(2){
 		potionName = _potionName;
 
+		if (strcmp(potionName.c_str(), "Potion")) {
+			healPokemonBy = 20;
+		}
+		else if (strcmp(potionName.c_str(), "SuperPotion")) {
+			healPokemonBy = 50;
+		}
+		else if (strcmp(potionName.c_str(), "HyperPotion")) {
+			healPokemonBy = 200;
+		}
+		else {
+			healPokemonBy = 0;
+		}
+
 	}
 
 	void useItem(Pokemon & pokemon) {
