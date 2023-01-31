8f4204a63b07d5d3f0d0b9a35ba9d12038bb5cb3
taylor92 <taylor92@lancaster.ac.uk>
2023-01-31 20:35:15 +0000

    Decided to just extend sprite

--- LeytonFinalProjectC++/Sprites/Items/Item.cpp
+++ LeytonFinalProjectC++/Sprites/Items/Item.cpp
@@ -1,12 +1,13 @@
-class Item  {
+#include "../../Sprites/SpriteHeader.h"
+
+class Item : Sprite  {
 public:
 
 	int idOfItem;
-	int xTile;
-	int yTile;
 
 
-	Item(int _idOfItem) {
+
+	Item(int _idOfItem, int _xStart, int _yStart) : Sprite(SpriteType::SpriteTypes::Item, _xStart, _yStart, 32, 32, "../../Sprites/Items/Items.png") {
 		idOfItem = _idOfItem;
 	}
 
