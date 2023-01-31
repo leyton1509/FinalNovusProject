a88e6f2e6d38415f5add87bbec0c8e46e72df4f3
taylor92 <taylor92@lancaster.ac.uk>
2023-01-31 20:36:40 +0000

    Added an x and y tile to the item class for the sprite sheet

--- LeytonFinalProjectC++/Sprites/Items/Item.cpp
+++ LeytonFinalProjectC++/Sprites/Items/Item.cpp
@@ -4,11 +4,14 @@ class Item : Sprite  {
 public:
 
 	int idOfItem;
+	int xTile;
+	int yTile;
 
 
-
-	Item(int _idOfItem, int _xStart, int _yStart) : Sprite(SpriteType::SpriteTypes::Item, _xStart, _yStart, 32, 32, "../../Sprites/Items/Items.png") {
+	Item(int _idOfItem, int _xStart, int _yStart, int _xTile, int _yTile) : Sprite(SpriteType::SpriteTypes::Item, _xStart, _yStart, 32, 32, "../../Sprites/Items/Items.png") {
 		idOfItem = _idOfItem;
+		xTile = _xTile;
+		yTile = _yTile;
 	}
 
 	void useItem() {
