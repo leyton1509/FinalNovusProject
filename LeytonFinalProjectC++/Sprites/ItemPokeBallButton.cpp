e6c6b00e032ec0397ad332326a5781698f043c2b
taylor92 <taylor92@lancaster.ac.uk>
2023-01-31 21:12:03 +0000

    Sets the pokeballs positional info in the const

--- LeytonFinalProjectC++/Sprites/ItemPokeBallButton.cpp
+++ LeytonFinalProjectC++/Sprites/ItemPokeBallButton.cpp
@@ -1,6 +1,7 @@
 #include "../Sprites/Button.h"
 #include "../Pokemon/Pokemon.h"
 #include <allegro5/allegro_font.h>
+#include "../Sprites/Items/PokeBall.h"
 
 // A specific attack button
 // Inherits from Button which inherits from Sprite
@@ -12,9 +13,16 @@ public:
 	// The font to draw with
 	ALLEGRO_FONT* fontSmaller = al_load_font("MagzoSemiBold-GOraO.otf", 16, NULL);
 
+	PokeBall pokeball;
+
 	// ItemPokeBallButton with the pokemon to add to the players party
 	ItemPokeBallButton(string _itemName, int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height) : Button(_originalSizeX, _originalSizeY, _xStart, _yStart, _width, _height, "../LeytonFinalProjectC++/Sprites/BattleSprites/SwitchPokemonButton.png") {
 		itemName = _itemName;
+		pokeball = PokeBall(_itemName);
+		pokeball.xPosition = _xStart;
+		pokeball.yPosition = _yStart;
+		pokeball.spritewidth = _width;
+		pokeball.spriteHeight = _height;
 	}
 
 	// Empty const
@@ -27,7 +35,7 @@ public:
 	// Draws the button and pokemon attacthed to it
 	void drawSprite(Pokemon& pokemon) {
 		al_draw_scaled_bitmap(spriteImage, 0, 0, originalSizeX, originalSizeY, xPosition, yPosition, spritewidth, spriteHeight, 0);
-		
+		pokeball.drawSprite();
 
 
 	}
