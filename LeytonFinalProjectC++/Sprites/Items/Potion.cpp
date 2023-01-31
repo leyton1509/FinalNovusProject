de58aea5145d087ee714d9c0d734cc266309e4b4
taylor92 <taylor92@lancaster.ac.uk>
2023-01-31 16:21:57 +0000

    Added an indivual id int

--- LeytonFinalProjectC++/Sprites/Items/Potion.cpp
+++ LeytonFinalProjectC++/Sprites/Items/Potion.cpp
@@ -9,11 +9,14 @@ public:
 
 	string potionName;
 	int healPokemonBy;
+	int inividualItemID;
+
 	Potion(string _potionName): Item(2){
 		potionName = _potionName;
 
 		if (strcmp(potionName.c_str(), "Potion")) {
 			healPokemonBy = 20;
+
 		}
 		else if (strcmp(potionName.c_str(), "SuperPotion")) {
 			healPokemonBy = 50;
