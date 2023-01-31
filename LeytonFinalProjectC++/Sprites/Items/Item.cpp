837168741b9f709f09bee546609745cdbe3c4f32
taylor92 <taylor92@lancaster.ac.uk>
2023-01-31 22:24:31 +0000

    Updated variable name with ForSpriteSheet

--- LeytonFinalProjectC++/Sprites/Items/Item.cpp
+++ LeytonFinalProjectC++/Sprites/Items/Item.cpp
@@ -4,18 +4,18 @@ class Item : public Sprite  {
 public:
 
 	int idOfItem;
-	int xTile;
-	int yTile;
+	int xTileForSpriteSheet;
+	int yTileForSpriteSheet;
 
 
 	Item(int _idOfItem, int _xStart, int _yStart, int _xTile, int _yTile) : Sprite(SpriteType::SpriteTypes::Item, _xStart, _yStart, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png") {
 		idOfItem = _idOfItem;
-		xTile = _xTile;
-		yTile = _yTile;
+		xTileForSpriteSheet = _xTile;
+		yTileForSpriteSheet = _yTile;
 	}
 
 	void drawSprite() {
-		al_draw_scaled_bitmap(spriteImage, (32 * xTile), (32 * yTile), 32, 32, xPosition,yPosition, spritewidth, spriteHeight, 0);
+		al_draw_scaled_bitmap(spriteImage, (32 * xTileForSpriteSheet), (32 * yTileForSpriteSheet), 32, 32, xPosition,yPosition, spritewidth, spriteHeight, 0);
 
 	}
 
