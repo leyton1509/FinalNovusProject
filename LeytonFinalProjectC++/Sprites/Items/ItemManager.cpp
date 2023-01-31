6d273c184bb00d983027efd08283bd6c68d86cdd
taylor92 <taylor92@lancaster.ac.uk>
2023-01-31 16:26:53 +0000

    Inserted the different item ids, currently has one of each to use

--- LeytonFinalProjectC++/Sprites/Items/ItemManager.cpp
+++ LeytonFinalProjectC++/Sprites/Items/ItemManager.cpp
@@ -8,5 +8,14 @@ public:
 	std::map<int, int> itemAmounts = {};
 
 	ItemManager(){
+		// 1 Pokeball | 2 GreatBall | 3 UltraBall 
+		// 4 Potion | 5 SuperPotion | 6 HyperPotion
+		itemAmounts.insert({ 1, 1 });
+		itemAmounts.insert({ 2, 1 });
+		itemAmounts.insert({ 3, 1 });
+		itemAmounts.insert({ 4, 1 });
+		itemAmounts.insert({ 5, 1 });
+		itemAmounts.insert({ 6, 1 });
+		
 	}
 };
