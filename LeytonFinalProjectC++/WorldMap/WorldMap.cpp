ae055c914c2a69049c7683755fd2464da563c664
taylor92 <taylor92@lancaster.ac.uk>
2023-01-31 10:34:59 +0000

    Added the correct size of building

--- LeytonFinalProjectC++/WorldMap/WorldMap.cpp
+++ LeytonFinalProjectC++/WorldMap/WorldMap.cpp
@@ -426,7 +426,7 @@ public:
 					al_draw_scaled_bitmap(mapTiles[textMap[i][j]], 0, 0, 254, 189, (0), (0), (32 * 13), (32 * 10), 0);
 				}
 				else if (textMap[i][j] == 20) {
-					al_draw_scaled_bitmap(mapTiles[textMap[i][j]], 0, 0, 159, 145, (0), (0), (32 * 13), (32 * 10), 0);
+					al_draw_scaled_bitmap(mapTiles[textMap[i][j]], 0, 0, 159, 145, (0), (0), (32 * 12), (32 * 8), 0);
 				}
 				else {
 					al_draw_bitmap(mapTiles[textMap[i][j]], i * 32, j * 32, 0);
