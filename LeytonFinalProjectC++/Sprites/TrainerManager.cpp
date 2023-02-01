9298bbdecd1d748b9e2f00ca77bbd6bcf0fffb79
taylor92 <taylor92@lancaster.ac.uk>
2023-02-01 12:57:58 +0000

    Added the trainers stylehseet bitmap

--- LeytonFinalProjectC++/Sprites/TrainerManager.cpp
+++ LeytonFinalProjectC++/Sprites/TrainerManager.cpp
@@ -1,11 +1,12 @@
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_image.h>
+#include "../Sprites/Spriteheader.h"
 
-class TrainerManager {
+class TrainerManager{
 
 public :
 
-	ALLEGRO_BITMAP* trainerStyleSheet = al_load_bitmap("../LeytonFinalProjectC++/Sprites/PlayerSprites/Trainers.png");
+	ALLEGRO_BITMAP * trainerStyleSheet = al_load_bitmap("../LeytonFinalProjectC++/Sprites/PlayerSprites/Trainers.png");
 
 	TrainerManager() {
 
