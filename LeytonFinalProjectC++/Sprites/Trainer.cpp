1975e3641ba864116eae30b562ac3f3d7afe26e9
taylor92 <taylor92@lancaster.ac.uk>
2023-02-01 15:35:20 +0000

    Added a pointer to the trainer name

--- LeytonFinalProjectC++/Sprites/Trainer.cpp
+++ LeytonFinalProjectC++/Sprites/Trainer.cpp
@@ -5,7 +5,7 @@ class Trainer : public Sprite{
 public:
 
 	
-	Trainer(int xTile, int yTile, ALLEGRO_BITMAP * stylesheet) : Sprite(SpriteType::SpriteTypes::Player, xTile * 32, yTile * 32,0,0, 0,0, 32,32, stylesheet) {
+	Trainer(const char * name, int xTile, int yTile, ALLEGRO_BITMAP * stylesheet) : Sprite(SpriteType::SpriteTypes::Player, xTile * 32, yTile * 32,0,0, 0,0, 32,32, stylesheet) {
 
 	}
 };
