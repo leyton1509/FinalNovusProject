daa227b8a90fbe3d452056595a6b3b60b4d6635f
taylor92 <taylor92@lancaster.ac.uk>
2023-02-01 12:21:36 +0000

    Added test interacble for pokeball to confirm it works

--- LeytonFinalProjectC++/Sprites/InteractablesForMaps.cpp
+++ LeytonFinalProjectC++/Sprites/InteractablesForMaps.cpp
@@ -12,11 +12,14 @@ public:
 			Interactable bf = Interactable(1, true, 220, 165, 15 * 32, 24 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/MapSprites/Case.png");
 			interactbles.push_front(bf);
 
-			Interactable potion = Interactable(2, true, 15 * 32, 22 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 15,0);
-			interactbles.push_front(potion);
+			//Interactable potion = Interactable(2, true, 15 * 32, 22 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 15,0);
+			//interactbles.push_front(potion);
 
-			Interactable superPotion = Interactable(3, true, 15 * 32, 20 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 24, 0);
-			interactbles.push_front(superPotion);
+			//Interactable superPotion = Interactable(3, true, 15 * 32, 20 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 24, 0);
+			//interactbles.push_front(superPotion);
+
+			Interactable pokeball = Interactable(5, true, 15 * 32, 20 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 3, 0);
+			interactbles.push_front(pokeball);
 
 			return interactbles;
 		}
