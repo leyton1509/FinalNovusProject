ca5bc52836b941cef25d6cf1b4202e06fc74ad84
taylor92 <taylor92@lancaster.ac.uk>
2023-02-01 15:34:54 +0000

    Added the successful constructor for sprite

--- LeytonFinalProjectC++/Sprites/Trainer.cpp
+++ LeytonFinalProjectC++/Sprites/Trainer.cpp
@@ -1,10 +1,11 @@
 #include "../Sprites/Spriteheader.h"
 
 
-class Trainer : public Sprite(){
+class Trainer : public Sprite{
 public:
+
 	
-	Trainer() {
+	Trainer(int xTile, int yTile, ALLEGRO_BITMAP * stylesheet) : Sprite(SpriteType::SpriteTypes::Player, xTile * 32, yTile * 32,0,0, 0,0, 32,32, stylesheet) {
 
 	}
 };
