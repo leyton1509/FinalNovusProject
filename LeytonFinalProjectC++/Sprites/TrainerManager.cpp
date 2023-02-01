bf11a7f3dfea6d941400b8c50a648d26c144e8d5
taylor92 <taylor92@lancaster.ac.uk>
2023-02-01 16:00:22 +0000

    Added a map for the trainers which maps a trainer to the trainer class

--- LeytonFinalProjectC++/Sprites/TrainerManager.cpp
+++ LeytonFinalProjectC++/Sprites/TrainerManager.cpp
@@ -1,12 +1,16 @@
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_image.h>
 #include "../Sprites/Spriteheader.h"
+#include <map>
+#include "../Sprites/Trainer.h"
 
 class TrainerManager{
 
 public :
 
 	ALLEGRO_BITMAP * trainerStyleSheet = al_load_bitmap("../LeytonFinalProjectC++/Sprites/PlayerSprites/Trainers.png");
+
+	std::map<int, Trainer> trainers = {};
 
 	TrainerManager() {
 
