9817c3f0b5b3aecda5f8395d13679be6425fc60e
taylor92 <taylor92@lancaster.ac.uk>
2023-02-01 11:26:01 +0000

    Updated item with new sizes

--- LeytonFinalProjectC++/Sprites/Items/Item.cpp
+++ LeytonFinalProjectC++/Sprites/Items/Item.cpp
@@ -8,14 +8,14 @@ public:
 	int yTileForSpriteSheet;
 
 
-	Item(int _idOfItem, int _xStart, int _yStart, int _xTile, int _yTile) : Sprite(SpriteType::SpriteTypes::Item, _xStart, _yStart, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png") {
+	Item(int _idOfItem, int _xStart, int _yStart, int _xTile, int _yTile) : Sprite(SpriteType::SpriteTypes::Item, _xStart, _yStart, 64, 64, "../LeytonFinalProjectC++/Sprites/Items/Items.png") {
 		idOfItem = _idOfItem;
 		xTileForSpriteSheet = _xTile;
 		yTileForSpriteSheet = _yTile;
 	}
 
 	void drawSprite() {
-		al_draw_scaled_bitmap(spriteImage, (32 * xTileForSpriteSheet), (32 * yTileForSpriteSheet), 32, 32, xPosition+20,yPosition+15, spritewidth /2, spriteHeight/2, 0);
+		al_draw_scaled_bitmap(spriteImage, (64 * xTileForSpriteSheet), (64 * yTileForSpriteSheet), 64, 64, xPosition+20,yPosition+15, spritewidth /2, spriteHeight/2, 0);
 
 	}
 
