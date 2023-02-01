c816a9796dff2cc27cd09572cb5a4c6bf52068de
taylor92 <taylor92@lancaster.ac.uk>
2023-02-01 12:55:08 +0000

    Added the spritesheet pointer

--- LeytonFinalProjectC++/Sprites/TrainerManager.cpp
+++ LeytonFinalProjectC++/Sprites/TrainerManager.cpp
@@ -1,6 +1,11 @@
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_image.h>
+
 class TrainerManager {
 
 public :
+
+	ALLEGRO_BITMAP* trainerStyleSheet = al_load_bitmap("../LeytonFinalProjectC++/Sprites/PlayerSprites/Trainers.png");
 
 	TrainerManager() {
 
