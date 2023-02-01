2c91091cc4cf1581ede10c5c19c9aa7db8c43b65
taylor92 <taylor92@lancaster.ac.uk>
2023-02-01 15:46:50 +0000

    Added comments

--- LeytonFinalProjectC++/Sprites/Trainer.cpp
+++ LeytonFinalProjectC++/Sprites/Trainer.cpp
@@ -4,13 +4,18 @@
 class Trainer : public Sprite{
 public:
 
+	// Name of the trainer
 	const char* name;
+	// X tile for on the map
 	int xTile;
+	// Y tile for on the map
 	int yTile;
+	// X tile for the style sheet position
 	int xTileForSpriteSheet;
+	// Y tile for the style sheet position
 	int yTileForSpriteSheet;
 
-	
+	// Main constructor for loading a character 
 	Trainer(const char * _name, int _xTile, int _yTile, ALLEGRO_BITMAP * stylesheet, int _xTileForSpriteSheet, int _yTileForSpriteSheet) : Sprite(SpriteType::SpriteTypes::Player, _xTile * 32, _yTile * 32,0,0, 0,0, 32,32, stylesheet) {
 		name = _name;
 		xTile = _xTile;
