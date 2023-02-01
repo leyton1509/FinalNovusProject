d91ab8230786e993edea946ea68d36616b302ded
taylor92 <taylor92@lancaster.ac.uk>
2023-02-01 15:41:00 +0000

    Added the x and y tile sheet ints

--- LeytonFinalProjectC++/Sprites/Trainer.cpp
+++ LeytonFinalProjectC++/Sprites/Trainer.cpp
@@ -7,11 +7,18 @@ public:
 	const char* name;
 	int xTile;
 	int yTile;
+	int xTileForSpriteSheet;
+	int yTileForSpriteSheet;
 
 	
-	Trainer(const char * _name, int _xTile, int _yTile, ALLEGRO_BITMAP * stylesheet) : Sprite(SpriteType::SpriteTypes::Player, _xTile * 32, _yTile * 32,0,0, 0,0, 32,32, stylesheet) {
+	Trainer(const char * _name, int _xTile, int _yTile, ALLEGRO_BITMAP * stylesheet, int _xTileForSpriteSheet, int _yTileForSpriteSheet) : Sprite(SpriteType::SpriteTypes::Player, _xTile * 32, _yTile * 32,0,0, 0,0, 32,32, stylesheet) {
 		name = _name;
 		xTile = _xTile;
 		yTile = _yTile;
+		xTileForSpriteSheet = _xTileForSpriteSheet;
+		yTileForSpriteSheet = _yTileForSpriteSheet;
+
 	}
+
+	
 };
