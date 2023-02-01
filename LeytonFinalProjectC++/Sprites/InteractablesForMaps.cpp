3dc43bf544ae7d3613eea200c725317d09ad9633
taylor92 <taylor92@lancaster.ac.uk>
2023-02-01 12:19:32 +0000

    Added two potions to the map for test

--- LeytonFinalProjectC++/Sprites/InteractablesForMaps.cpp
+++ LeytonFinalProjectC++/Sprites/InteractablesForMaps.cpp
@@ -12,8 +12,11 @@ public:
 			Interactable bf = Interactable(1, true, 220, 165, 15 * 32, 24 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/MapSprites/Case.png");
 			interactbles.push_front(bf);
 
-			Interactable bf = Interactable(2, true, 15 * 32, 22 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 0,0);
-			interactbles.push_front(bf);
+			Interactable potion = Interactable(2, true, 15 * 32, 22 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 15,0);
+			interactbles.push_front(potion);
+
+			Interactable superPotion = Interactable(3, true, 15 * 32, 20 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 24, 0);
+			interactbles.push_front(superPotion);
 
 			return interactbles;
 		}
