c3bc1c03897ee1da0ae16fbd40ebfe54a23f6add
taylor92 <taylor92@lancaster.ac.uk>
2023-02-02 11:49:49 +0000

    Added rival trainer to manager

--- LeytonFinalProjectC++/Sprites/TrainerManager.cpp
+++ LeytonFinalProjectC++/Sprites/TrainerManager.cpp
@@ -29,6 +29,7 @@ public :
 	TrainerManager() {
 		// const char * _name, int _xTile, int _yTile, ALLEGRO_BITMAP * stylesheet, int _xTileForSpriteSheet, int _yTileForSpriteSheet
 		trainers.insert({ 1, Trainer("Elm", 10, 10, trainerStyleSheet, 0, 0) });
+		trainers.insert({ 2, Trainer("Rival", 10, 10, trainerStyleSheet, 3, 0) });
 	}
 
 	Trainer getTrainer(int trainerNum) {
