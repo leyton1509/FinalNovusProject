c85398cd491d59274258cf23324f9b83cd41f997
taylor92 <taylor92@lancaster.ac.uk>
2023-02-02 23:06:29 +0000

    Added code from switch pokeon but removed unneccasry parts

---
+++ LeytonFinalProjectC++/Sprites/SwitchPokemonPCButton.cpp
@@ -0,0 +1,47 @@
+#include "../Sprites/Button.h"
+#include "../Pokemon/Pokemon.h"
+#include <allegro5/allegro_font.h>
+
+// A specific attack button
+// Inherits from Button which inherits from Sprite
+class SwitchPokemonPCButton : public Button {
+
+public:
+
+	ALLEGRO_BITMAP* otherPokemonSprite = al_load_bitmap("../LeytonFinalProjectC++/Sprites/PokemonSprites/frontSprites.png");
+
+	// The original x size of button image
+	int originalSizeX;
+	// The original y size of button image
+	int originalSizeY;
+
+
+	// Connstructor with the pokemon to add to the players party
+	SwitchPokemonPCButton(int _originalSizeX, int _originalSizeY, float _xStart, float _yStart, float _width, float _height) : Button(_originalSizeX, _originalSizeY, _xStart, _yStart, _width, _height, "../LeytonFinalProjectC++/Sprites/BattleSprites/SwitchPokemonButton.png") {
+		originalSizeX = _originalSizeX;
+		originalSizeY = _originalSizeY;
+	}
+
+	// Empty const
+	SwitchPokemonPCButton() : Button() {
+		originalSizeX = 0;
+		originalSizeY = 0;
+	}
+
+	// Draws the button and pokemon attacthed to it
+	void drawSprite(Pokemon& pokemon) {
+		al_draw_scaled_bitmap(spriteImage, 0, 0, originalSizeX, originalSizeY, xPosition, yPosition, spritewidth, spriteHeight, 0);
+		al_draw_scaled_bitmap(otherPokemonSprite, (80 * pokemon.xPositionOnSpriteSheet), (80 * pokemon.yPositionOnSpriteSheet), 80, 80, xPosition + 4, yPosition - 2, spritewidth * 0.9, spriteHeight * 0.9, 0);
+	}
+
+	void destroySprite() {
+		al_destroy_bitmap(spriteImage);
+		al_destroy_bitmap(otherPokemonSprite);
+	}
+
+
+
+
+
+
+};
