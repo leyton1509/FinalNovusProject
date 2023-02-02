cdae85d3fa551958ccc6fb65c445dc686fabadd1
taylor92 <taylor92@lancaster.ac.uk>
2023-02-02 10:31:06 +0000

    Added two varibales, a and y original tile, as the original tile needs to be stored

--- LeytonFinalProjectC++/Sprites/Trainer.cpp
+++ LeytonFinalProjectC++/Sprites/Trainer.cpp
@@ -16,11 +16,18 @@ public:
 	// Y tile for the style sheet position
 	int yTileForSpriteSheet;
 
+	// original x for tile
+	int xTileOriginal;
+	// original y for tile
+	int yTileForOriginal;
+
 	bool hasBeenFought = false;
 
 	// Main constructor for loading a character 
 	Trainer(const char * _name, int _xTile, int _yTile, ALLEGRO_BITMAP * stylesheet, int _xTileForSpriteSheet, int _yTileForSpriteSheet) : Sprite(SpriteType::SpriteTypes::Player, _xTile * 32, _yTile * 32,0,0, 0,0, 32,32, stylesheet) {
 		name = _name;
+		xTileOriginal = _xTile;
+		yTileForOriginal = _yTile;
 		xTile = _xTile;
 		yTile = _yTile;
 		xTileForSpriteSheet = _xTileForSpriteSheet;
@@ -31,6 +38,8 @@ public:
 		name = "";
 		xTile = 0;
 		yTile = 0;
+		xTileOriginal = 0;
+		yTileForOriginal = 0;
 		xTileForSpriteSheet = 0;
 		yTileForSpriteSheet = 0;
 	}
