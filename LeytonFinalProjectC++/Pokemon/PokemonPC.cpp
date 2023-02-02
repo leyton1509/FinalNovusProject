cd9c6616c4b3e431970b71a93d1c27a26c0f62e9
taylor92 <taylor92@lancaster.ac.uk>
2023-02-02 23:20:32 +0000

    Displays the box pokemon

--- LeytonFinalProjectC++/Pokemon/PokemonPC.cpp
+++ LeytonFinalProjectC++/Pokemon/PokemonPC.cpp
@@ -92,6 +92,7 @@ public:
 			{
 				al_clear_to_color(al_map_rgb(0, 0, 0));
 				al_draw_scaled_bitmap(background, 0, 0, 321, 240, 0, 0, screenWidth, screenHeight, 0);
+				displaySprites(player.box);
 				al_flip_display();
 			}
 
