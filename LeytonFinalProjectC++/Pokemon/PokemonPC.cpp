f39380c16ae4ba538ce08c8aca830e040e632f2e
taylor92 <taylor92@lancaster.ac.uk>
2023-02-02 22:41:48 +0000

    Created a for loop to run through the creation of buttons

--- LeytonFinalProjectC++/Pokemon/PokemonPC.cpp
+++ LeytonFinalProjectC++/Pokemon/PokemonPC.cpp
@@ -10,7 +10,14 @@ public:
 
 	PokemonPC(PlayerCharacter & player, int screenWidth, int screenHeight) {
 
+		for (int i = 0; i < 6; i++)
+		{
+			for (int j = 0; j < 5; j++)
+			{
 
+			}
+
+		}
 
 	}
 };
