463cac091cb530a168cacb6d7fce5faca1c42cbe
taylor92 <taylor92@lancaster.ac.uk>
2023-02-02 22:43:28 +0000

    Added a starting x and y position

--- LeytonFinalProjectC++/Pokemon/PokemonPC.cpp
+++ LeytonFinalProjectC++/Pokemon/PokemonPC.cpp
@@ -10,10 +10,14 @@ public:
 
 	PokemonPC(PlayerCharacter & player, int screenWidth, int screenHeight) {
 
+		int startXPositon = 10;
+		int startYPosition = 10;
+
 		for (int i = 0; i < 6; i++)
 		{
 			for (int j = 0; j < 5; j++)
 			{
+				
 
 			}
 
