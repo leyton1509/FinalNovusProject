f553c26af74c701a51bf3baa2ee7a0e4c02016b7
taylor92 <taylor92@lancaster.ac.uk>
2023-02-02 11:57:59 +0000

    Added the next ten trainers

--- LeytonFinalProjectC++/Sprites/TrainerManager.cpp
+++ LeytonFinalProjectC++/Sprites/TrainerManager.cpp
@@ -17,7 +17,7 @@ public :
 
 		std::list<Trainer> trainerList = {};
 		if (mapNumber==1) {
-			Trainer t1 = getTrainer(1);
+			Trainer t1 = getTrainer(11);
 			t1.setPositions(15, 12, 0, 1);
 			trainerList.push_front(t1);
 		}
@@ -39,16 +39,16 @@ public :
 		trainers.insert({ 9, Trainer("Bug Catcher", 1, 1, trainerStyleSheet, 24, 0) });
 		trainers.insert({ 10, Trainer("Young Man", 1, 1, trainerStyleSheet, 27, 0) });
 
-		trainers.insert({ 11, Trainer("Elm", 1, 1, trainerStyleSheet, 0, 0) });
-		trainers.insert({ 12, Trainer("Rival", 1, 1, trainerStyleSheet, 3, 0) });
-		trainers.insert({ 13, Trainer("Steven", 1, 1, trainerStyleSheet, 6, 0) });
-		trainers.insert({ 14, Trainer("Old Woman", 1, 1, trainerStyleSheet, 9, 0) });
-		trainers.insert({ 15, Trainer("Old Woman", 1, 1, trainerStyleSheet, 12, 0) });
-		trainers.insert({ 16, Trainer("Small Boy", 1, 1, trainerStyleSheet, 15, 0) });
-		trainers.insert({ 17, Trainer("Small Girl", 1, 1, trainerStyleSheet, 18, 0) });
-		trainers.insert({ 18, Trainer("Joey", 1, 1, trainerStyleSheet, 21, 0) });
-		trainers.insert({ 19, Trainer("Bug Catcher", 1, 1, trainerStyleSheet, 24, 0) });
-		trainers.insert({ 20, Trainer("Young Man", 1, 1, trainerStyleSheet, 27, 0) });
+		trainers.insert({ 11, Trainer("Emma", 1, 1, trainerStyleSheet, 0, 4) });
+		trainers.insert({ 12, Trainer("Jim", 1, 1, trainerStyleSheet, 3, 4) });
+		trainers.insert({ 13, Trainer("John", 1, 1, trainerStyleSheet, 6, 4) });
+		trainers.insert({ 14, Trainer("Max", 1, 1, trainerStyleSheet, 9, 4) });
+		trainers.insert({ 15, Trainer("Ace", 1, 1, trainerStyleSheet, 12, 4) });
+		trainers.insert({ 16, Trainer("Sammy", 1, 1, trainerStyleSheet, 15, 4) });
+		trainers.insert({ 17, Trainer("Abi", 1, 1, trainerStyleSheet, 18, 4) });
+		trainers.insert({ 18, Trainer("Female Ace", 1, 1, trainerStyleSheet, 21, 4) });
+		trainers.insert({ 19, Trainer("Old Man Tom", 1, 1, trainerStyleSheet, 24, 4) });
+		trainers.insert({ 20, Trainer("Cook Sammy", 1, 1, trainerStyleSheet, 27, 4) });
 	}
 
 	Trainer getTrainer(int trainerNum) {
