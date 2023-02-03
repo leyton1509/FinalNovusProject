48a51fd3cf6a43d2837f16d9e39feda60566c08e
taylor92 <taylor92@lancaster.ac.uk>
2023-02-03 15:49:05 +0000

    Added variables for starting x y and counter

--- LeytonFinalProjectC++/Sprites/ShopMenu.cpp
+++ LeytonFinalProjectC++/Sprites/ShopMenu.cpp
@@ -7,5 +7,12 @@ public:
 
 	ShopMenu(bool buy, PlayerCharacter & player, int screenWidth, int screenHeight, ALLEGRO_EVENT_QUEUE* queue) {
 
+		int startXPositon = 10;
+		int startYPosition = 10;
+		int counter = 0;
+
+
+
+
 	}
 };
