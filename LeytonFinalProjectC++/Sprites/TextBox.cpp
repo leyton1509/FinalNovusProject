dfca498f3de793130d21279ab100bd34ac9953e7
taylor92 <taylor92@lancaster.ac.uk>
2023-02-03 11:18:39 +0000

    Added comments

--- LeytonFinalProjectC++/Sprites/TextBox.cpp
+++ LeytonFinalProjectC++/Sprites/TextBox.cpp
@@ -15,11 +15,13 @@ public:
 	// The font to display with
 	ALLEGRO_FONT* font = al_load_font("MagzoSemiBold-GOraO.otf", 28, NULL);
 
+	// Constructor for text box
 	TextBox(std::list<std::string> textToDisplay, int _width, int _height) : Sprite (SpriteType::SpriteTypes::Button, 10, 450, _width, _height, "../LeytonFinalProjectC++/Sprites/TextBox/TextBox.png"){
 		originalSizeX = 256;
 		originalSizeY = 96;
 	}
 
+	// Draws the sprite with the string
 	void drawSprite(std::string currentString) {
 		al_draw_scaled_bitmap(spriteImage, 0, 0, originalSizeX, originalSizeY, xPosition, yPosition, spritewidth, spriteHeight, 0);
 		al_draw_text(font, al_map_rgb(255, 255, 255), (xPosition + 15), (yPosition + 5), 0, (currentString).c_str());
