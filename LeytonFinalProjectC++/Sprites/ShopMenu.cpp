cbe63025147fe1db0d7bffb43dc5d7e8eaa55468
taylor92 <taylor92@lancaster.ac.uk>
2023-02-03 17:50:12 +0000

    Added the buy buttons

--- LeytonFinalProjectC++/Sprites/ShopMenu.cpp
+++ LeytonFinalProjectC++/Sprites/ShopMenu.cpp
@@ -36,7 +36,14 @@ public:
 		ItemPotionButton potionButtonTwo = ItemPotionButton(5, 64, 64, 10, 370, 80, 80);
 		ItemPotionButton potionButtonThree = ItemPotionButton(6, 64, 64, 10, 460, 80, 80);
 
-		Button exitButton = Button(128, 128, 190, 460, 80, 80, "../LeytonFinalProjectC++/Sprites/PCSprites/Exit.png");
+		Button exitButton = Button(128, 128, 280, 460, 80, 80, "../LeytonFinalProjectC++/Sprites/PCSprites/Exit.png");
+
+		Button buyButtonOne = Button(128, 128, 100, 10, 80, 80, "../LeytonFinalProjectC++/Sprites/PCSprites/Exit.png", 200);
+		Button buyButtonTwo = Button(128, 128, 100, 100, 80, 80, "../LeytonFinalProjectC++/Sprites/PCSprites/Exit.png", 400);
+		Button buyButtonThree = Button(128, 128, 100, 190, 80, 80, "../LeytonFinalProjectC++/Sprites/PCSprites/Exit.png", 800);
+		Button buyButtonOne = Button(128, 128, 100, 280, 80, 80, "../LeytonFinalProjectC++/Sprites/PCSprites/Exit.png", 250);
+		Button buyButtonTwo = Button(128, 128, 100, 370, 80, 80, "../LeytonFinalProjectC++/Sprites/PCSprites/Exit.png", 450);
+		Button buyButtonThree = Button(128, 128, 100, 460, 80, 80, "../LeytonFinalProjectC++/Sprites/PCSprites/Exit.png", 850);
 
 		bool finishedInShop = false;
 
