4df42976155e5a9acc636371f2a477e226ce934f
taylor92 <taylor92@lancaster.ac.uk>
2023-02-03 15:56:17 +0000

    Added the number of items and a loop for the colmuns

--- LeytonFinalProjectC++/Sprites/ShopMenu.cpp
+++ LeytonFinalProjectC++/Sprites/ShopMenu.cpp
@@ -1,17 +1,27 @@
 #include "../Sprites/PlayerCharacter.h"
 
+// Menu for handling buying / selling items
 class ShopMenu {
 
 
 public:
 
+	// Constructor for handling buying / selling items
 	ShopMenu(bool buy, PlayerCharacter & player, int screenWidth, int screenHeight, ALLEGRO_EVENT_QUEUE* queue) {
 
 		int startXPositon = 10;
 		int startYPosition = 10;
 		int counter = 0;
 
+		// Gets the number of items
+		int nOfItems = player.itemManager.getNumberOfItems();
+		// Work out how many columns to displau
+		int nOfColumns = (nOfItems / 8) + 1;
 
+		for (int i = 0; i < nOfColumns; i++)
+		{
+
+		}
 
 
 	}
