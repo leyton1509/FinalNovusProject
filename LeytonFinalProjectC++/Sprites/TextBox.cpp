28e686714c6a9d7274aa7a081af9d300f34b15de
taylor92 <taylor92@lancaster.ac.uk>
2023-02-03 11:15:11 +0000

    Added font

--- LeytonFinalProjectC++/Sprites/TextBox.cpp
+++ LeytonFinalProjectC++/Sprites/TextBox.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include "../Sprites/Spriteheader.h"
 
+// Class to display text to the user in the over world
 class TextBox : public Sprite {
 
 public: 
@@ -11,6 +12,8 @@ public:
 	int originalSizeX;
 	// Original Y Size of image
 	int originalSizeY;
+	// The font to display with
+	ALLEGRO_FONT* font = al_load_font("MagzoSemiBold-GOraO.otf", 28, NULL);
 
 	TextBox(std::list<std::string> textToDisplay, int _width, int _height) : Sprite (SpriteType::SpriteTypes::Button, 10, 450, _width, _height, "../LeytonFinalProjectC++/Sprites/TextBox/TextBox.png"){
 		originalSizeX = 256;
