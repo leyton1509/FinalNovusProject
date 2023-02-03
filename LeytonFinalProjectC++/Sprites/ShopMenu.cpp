85471923282dbeee72b0747dd273af43a6121a5d
taylor92 <taylor92@lancaster.ac.uk>
2023-02-03 16:28:50 +0000

    Added skeleton allgero gui controller

--- LeytonFinalProjectC++/Sprites/ShopMenu.cpp
+++ LeytonFinalProjectC++/Sprites/ShopMenu.cpp
@@ -15,6 +15,13 @@ public:
 		int startYPosition = 10;
 		int counter = 0;
 
+		// GUI info
+		bool done = false;
+		bool redraw = true;
+		ALLEGRO_EVENT event;
+		int xMousePosition = 0;
+		int yMousePosition = 0;
+
 		ItemPokeBallButton pokeballButtonOne = ItemPokeBallButton(1, 64, 64, 10, 90, 80, 80);
 		ItemPokeBallButton pokeballButtonTwo = ItemPokeBallButton(2, 64, 64, 10, 180, 80, 80);
 		ItemPokeBallButton pokeballButtonThree = ItemPokeBallButton(3, 64, 64, 10, 270, 80, 80);
@@ -22,6 +29,46 @@ public:
 		ItemPotionButton potionButtonTwo = ItemPotionButton(5, 64, 64, 630, 450, 80, 80);
 		ItemPotionButton potionButtonThree = ItemPotionButton(6, 64, 64, 720, 540, 80, 80);
 
+		bool finishedInPC = false;
+
+		while (!finishedInPC) {
+
+			al_wait_for_event(queue, &event);
+
+			switch (event.type)
+			{
+			case ALLEGRO_EVENT_MOUSE_AXES:
+				xMousePosition = event.mouse.x;
+				yMousePosition = event.mouse.y;
+				break;
+			case ALLEGRO_EVENT_TIMER:
+
+
+				redraw = true;
+				break;
+
+			case ALLEGRO_EVENT_DISPLAY_CLOSE:
+				done = true;
+				finishedInPC = true;
+				break;
+			case ALLEGRO_EVENT_MOUSE_BUTTON_UP:
+				break;
+			}
+
+			if (done) {
+				break;
+			}
+				
+
+			if (redraw && al_is_event_queue_empty(queue))
+			{
+				al_clear_to_color(al_map_rgb(0, 0, 0));
+				
+				al_flip_display();
+			}
+
+
+		}
 
 		
 		pokeballButtonOne.drawSprite(player.itemManager.getAmountOfItem(pokeballButtonOne.pokeball.inividualItemID));
