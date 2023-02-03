183568015513514738bc0cb7923f8cd64c25f3c2
taylor92 <taylor92@lancaster.ac.uk>
2023-02-03 11:14:32 +0000

    Added original size of image

--- LeytonFinalProjectC++/Sprites/TextBox.cpp
+++ LeytonFinalProjectC++/Sprites/TextBox.cpp
@@ -7,7 +7,13 @@ class TextBox : public Sprite {
 
 public: 
 
-	TextBox(std::list<std::string> textToDisplay, int _width, int _height) : Sprite (SpriteType::SpriteTypes::Button, 10, 450, _width, _height, "../LeytonFinalProjectC++/Sprites/TextBox/TextBox.png"){
+	// Original X Size of image
+	int originalSizeX;
+	// Original Y Size of image
+	int originalSizeY;
 
+	TextBox(std::list<std::string> textToDisplay, int _width, int _height) : Sprite (SpriteType::SpriteTypes::Button, 10, 450, _width, _height, "../LeytonFinalProjectC++/Sprites/TextBox/TextBox.png"){
+		originalSizeX = 256;
+		originalSizeY = 96;
 	}
 };
