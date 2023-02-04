d70206ffd83725d0ff58ac0f79d24896d55b5d5b
taylor92 <taylor92@lancaster.ac.uk>
2023-02-04 16:37:58 +0000

    There is an issue if the pokemon evolves and learns a move on the same turn

--- LeytonFinalProjectC++/Battle/InitiateBattle.cpp
+++ LeytonFinalProjectC++/Battle/InitiateBattle.cpp
@@ -416,7 +416,7 @@ public:
 
 								if (moveInfo.first != -1) {
 									Move move = mm.getMoveDetails(moveInfo.first);
-									string headerText = player.trainersParty[currentPokemon].pokemonName + " is learning " + move.moveName;
+									string headerText = player.trainersParty[currentPokemon].pokemonName + " is learning    " + move.moveName;
 									std::list<std::string> buttonStrings = {};
 									buttonStrings.push_back("Replace " + player.trainersParty[currentPokemon].pokemonsMoves[0].moveName);
 									buttonStrings.push_back("Replace " + player.trainersParty[currentPokemon].pokemonsMoves[1].moveName);
@@ -928,7 +928,7 @@ public:
 
 								if (moveInfo.first != -1) {
 									Move move = mm.getMoveDetails(moveInfo.first);
-									string headerText = player.trainersParty[currentPokemon].pokemonName + " is learning " + move.moveName;
+									string headerText = player.trainersParty[currentPokemon].pokemonName + " is learning    " + move.moveName;
 									std::list<std::string> buttonStrings = {};
 									buttonStrings.push_back("Replace " + player.trainersParty[currentPokemon].pokemonsMoves[0].moveName);
 									buttonStrings.push_back("Replace " + player.trainersParty[currentPokemon].pokemonsMoves[1].moveName);
