f11e6e17e18475b0ee30bc26029d82ae839143f0
taylor92 <taylor92@lancaster.ac.uk>
2023-02-04 14:59:57 +0000

    Added an array of buttons

--- LeytonFinalProjectC++/Sprites/UserOption.cpp
+++ LeytonFinalProjectC++/Sprites/UserOption.cpp
@@ -1,9 +1,13 @@
 #include <allegro5/allegro_font.h>
+#include "../Sprites/Button.h"
 
+// A class which will allow the user to press an option
 class UserOption {
 
 public:
 
+	// The buttons for the options. Max of 6 options
+	Button buttonOptions[6];
 	// The font to write with
 	ALLEGRO_FONT* font = al_load_font("MagzoSemiBold-GOraO.otf", 16, NULL);
 
