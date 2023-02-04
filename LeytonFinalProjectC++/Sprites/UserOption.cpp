9f0b0349d850c126c7625d26c4d93c7a94bacfda
taylor92 <taylor92@lancaster.ac.uk>
2023-02-04 14:50:35 +0000

    Added the font

--- LeytonFinalProjectC++/Sprites/UserOption.cpp
+++ LeytonFinalProjectC++/Sprites/UserOption.cpp
@@ -1,6 +1,11 @@
+#include <allegro5/allegro_font.h>
+
 class UserOption {
 
 public:
+
+	// The font to write with
+	ALLEGRO_FONT* font = al_load_font("MagzoSemiBold-GOraO.otf", 16, NULL);
 
 	UserOption() {
 
