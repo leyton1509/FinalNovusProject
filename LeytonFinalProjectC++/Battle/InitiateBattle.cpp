46fb1f5ee61ffe4226ad4ad321d037ff60ddea76
taylor92 <taylor92@lancaster.ac.uk>
2023-02-04 16:08:52 +0000

    Added the move name and widened the button width

--- LeytonFinalProjectC++/Battle/InitiateBattle.cpp
+++ LeytonFinalProjectC++/Battle/InitiateBattle.cpp
@@ -410,11 +410,11 @@ public:
 									Move move = mm.getMoveDetails(moveInfo.first);
 									string headerText = player.trainersParty[currentPokemon].pokemonName + " is learning " + move.moveName;
 									std::list<std::string> buttonStrings = {};
-									buttonStrings.push_back("Replace Move 1");
-									buttonStrings.push_back("Replace Move 2");
-									buttonStrings.push_back("Replace Move 3");
-									buttonStrings.push_back("Replace Move 4");
-									UserOption us = UserOption(headerText,buttonStrings, 4, 120, 30, screenWidth, screenHeight, queue);
+									buttonStrings.push_back("Replace " + player.trainersParty[currentPokemon].pokemonsMoves[0].moveName);
+									buttonStrings.push_back("Replace " + player.trainersParty[currentPokemon].pokemonsMoves[1].moveName);
+									buttonStrings.push_back("Replace " + player.trainersParty[currentPokemon].pokemonsMoves[2].moveName);
+									buttonStrings.push_back("Replace " + player.trainersParty[currentPokemon].pokemonsMoves[3].moveName);
+									UserOption us = UserOption(headerText,buttonStrings, 4, 180, 30, screenWidth, screenHeight, queue);
 									int positionToPutMove = us.valueOfResult;
 									if (positionToPutMove != -1 && positionToPutMove < 4) {
 										player.trainersParty[currentPokemon].pokemonsMoves[positionToPutMove] = move;
