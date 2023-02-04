ba57aa87d1bbd8eea3d6c074537d97f55124b315
taylor92 <taylor92@lancaster.ac.uk>
2023-02-04 15:49:42 +0000

    Changed button to be thinner

--- LeytonFinalProjectC++/Sprites/UserOption.cpp
+++ LeytonFinalProjectC++/Sprites/UserOption.cpp
@@ -34,7 +34,7 @@ public:
 
 			for (int i = 0; i < numberOfOptions; i++)
 			{
-				buttonOptions[i] = Button(128, 128, xStartPos, yStartPos + (90 * i), buttonWidth, buttonHeight, "../LeytonFinalProjectC++/Sprites/PCSprites/HighlightedPokemon.png", i);
+				buttonOptions[i] = Button(128, 128, xStartPos, yStartPos + (buttonHeight + (buttonHeight * 0.1) * i), buttonWidth, buttonHeight, "../LeytonFinalProjectC++/Sprites/PCSprites/HighlightedPokemon.png", i);
 
 			}
 
