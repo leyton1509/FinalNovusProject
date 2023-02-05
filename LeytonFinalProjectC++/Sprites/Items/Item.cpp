827398d5c427430b0f6c67db9d6f2dfdefd17bd6
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-05 19:48:57 +0000

    Added comments

--- LeytonFinalProjectC++/Sprites/Items/Item.cpp
+++ LeytonFinalProjectC++/Sprites/Items/Item.cpp
@@ -1,24 +1,30 @@
 #include "../../Sprites/SpriteHeader.h"
 
+// Class which represents an item the player can have
+// Extends sprite
 class Item : public Sprite  {
 public:
-
+	// The id of the item
 	int idOfItem;
+	// The xtile of the stylesheet
 	int xTileForSpriteSheet;
+	// The ytile of the stylesheet
 	int yTileForSpriteSheet;
 
-
+	// Constructor for item, takes the id and position as well as the style sheet position
 	Item(int _idOfItem, int _xStart, int _yStart, int _xTile, int _yTile) : Sprite(SpriteType::SpriteTypes::Item, _xStart, _yStart, 64, 64, "../LeytonFinalProjectC++/Sprites/Items/Items.png") {
 		idOfItem = _idOfItem;
 		xTileForSpriteSheet = _xTile;
 		yTileForSpriteSheet = _yTile;
 	}
 
+	// Draws the item
 	void drawSprite() {
 		al_draw_scaled_bitmap(spriteImage, (64 * xTileForSpriteSheet), (64 * yTileForSpriteSheet), 64, 64, xPosition+20,yPosition+15, spritewidth /2, spriteHeight/2, 0);
 
 	}
 
+	// Method for use item to be overridden
 	void useItem() {
 
 	}
