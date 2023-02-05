5ae09f76db49b59d1bd3d3226c040f173b38e0c6
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-05 22:47:29 +0000

    Added comments

--- LeytonFinalProjectC++/Pokemon/Moves/MoveManager.cpp
+++ LeytonFinalProjectC++/Pokemon/Moves/MoveManager.cpp
@@ -6,19 +6,25 @@
 #include "../../Pokemon/Moves/MoveCategory.h"
 using namespace std;
 
-
+// Class to handle all the moves in the game
+// Is a singleton as once created it holds all moves
 class MoveManager
 {
 
 private:
     MoveManager() {
+        // Uses the csv reader to read a csv file of all moves in the game
         CSVFileManager csvm = CSVFileManager();
+        // Uses type and pokemon type for the move
         PokemonType pt = PokemonType();
         MoveCategory mc = MoveCategory();
 
+        // Filepath
          std::vector<std::vector<std::string> > cs = csvm.parseCSV("C:/Users/leyto/source/repos/LeytonFinalProjectC++/LeytonFinalProjectC++/CSVs/PokemonMoves.csv");
         //std::vector<std::vector<std::string> > cs = csvm.parseCSV("C:/Users/Owner/source/repos/FinalNovusProject/LeytonFinalProjectC++/CSVs/PokemonMoves.csv");
 
+         // Loops through the file
+         // Gets all the move info and inserts it into the map
         for (int i = 0; i < cs.size(); i++) {
             int moveID = std::stoi(cs.at(i)[0]);
             string moveName = cs.at(i)[1];
@@ -37,14 +43,17 @@ private:
 
 public:
 
+    // The instance of the class
     static MoveManager& instance()
     {
         static MoveManager INSTANCE;
         return INSTANCE;
     }
 
+    // The storage for the moves
     map<int, Move> moveStorage = {};
 
+    // Gets a move from its ID
     Move getMoveDetails(int moveID) {
 
         auto getter = moveStorage.find(moveID);
