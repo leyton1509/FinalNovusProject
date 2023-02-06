ea86fee4d9d5fc3d0c13d46bf614818573f849a9
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-06 11:56:44 +0000

    Added comment to class

--- LeytonFinalProjectC++/Battle/MoveAnimation/MoveAnimation.cpp
+++ LeytonFinalProjectC++/Battle/MoveAnimation/MoveAnimation.cpp
@@ -1,14 +1,25 @@
+
+// A class to control a single move animation
 class MoveAnimation {
 
+	// The current frame of the animation
 	int currentFrame;
+	// The starting X position of the animation
 	int startX;
+	// The starting X position of the animation
 	int startY;
+	// The destination X position of the animation
 	int destinationX;
+	// The destination Y position of the animation
 	int destinationY;
+	// The state of the animation
 	int state;
+	// The xtile for the style sheet
 	int xTileForStyleSheet;
+	// The ytile for the style sheet
 	int yStileForSyleSheet;
 
+	// Default creation of move animation
 	MoveAnimation() {
 
 	}
