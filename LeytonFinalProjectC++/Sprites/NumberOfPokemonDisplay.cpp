751b9d8dfc3a14f235f24b4a2a513cba838e9bf5
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-06 22:12:55 +0000

    Added check for valid pokemon

--- LeytonFinalProjectC++/Sprites/NumberOfPokemonDisplay.cpp
+++ LeytonFinalProjectC++/Sprites/NumberOfPokemonDisplay.cpp
@@ -20,7 +20,12 @@ public:
 		for (int i = 0; i < 6; i++)
 		{
 			al_draw_circle((startX * i + 10), startY, radius, al_map_rgb(211,211,211), 1);
-			party[i];
+			if (!(strcmp(party[i].pokemonName.c_str(), "") == 0)) {
+
+
+
+			}
+			
 		}
 	}
 
