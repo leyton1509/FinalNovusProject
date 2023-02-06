757b42a4866fd2a0833f08ec69300806870dfecc
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-06 12:51:25 +0000

    Created a move animation when start is called

--- LeytonFinalProjectC++/Battle/MoveAnimation/MoveAnimationHandler.cpp
+++ LeytonFinalProjectC++/Battle/MoveAnimation/MoveAnimationHandler.cpp
@@ -1,8 +1,14 @@
 #include "../../Battle/MoveAnimation/MoveAnimation.h"
+#include <list>
 
+// Class to handle all move animation creation and finishing
 class MoveAnimationHandler {
 
-	void startAnimation() {
+
+	std::list<MoveAnimation> animations = {};
+
+	void startAnimation(int _moveID, int _playerPokemonX, int _playerPokemonY, int _opponentPokemonX, int _opponentPokemonY) {
+		MoveAnimation ani = MoveAnimation(_moveID, _playerPokemonX, _playerPokemonY,_opponentPokemonX, _opponentPokemonY);
 
 	}
 
