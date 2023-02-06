3744f122c085e9b5662e06192a423af22d4b1f0d
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-06 17:17:48 +0000

    Added a switch statement for all types

--- LeytonFinalProjectC++/Battle/MoveAnimation/MoveAnimation.cpp
+++ LeytonFinalProjectC++/Battle/MoveAnimation/MoveAnimation.cpp
@@ -59,6 +59,69 @@ public :
 		MoveManager moveManager = moveManager.instance();
 		Move move = moveManager.getMoveDetails(moveID);
 
+		switch (move.pokemonTypeOfMove)
+		{
+		case PokemonType::Normal:
+			
+			break;
+		case PokemonType::Fire:
+			
+			break;
+		case PokemonType::Water:
+			
+			break;
+		case PokemonType::Grass:
+			
+			break;
+		case PokemonType::Electric:
+			
+			break;
+		case PokemonType::Ice:
+			
+			break;
+		case PokemonType::Fighting:
+			
+			break;
+		case PokemonType::Poison:
+			
+			break;
+		case PokemonType::Ground:
+			
+			break;
+		case PokemonType::Flying:
+			
+			break;
+		case PokemonType::Psychic:
+			
+			break;
+		case PokemonType::Bug:
+			
+			break;
+		case PokemonType::Rock:
+			
+			break;
+		case PokemonType::Ghost:
+			
+			break;
+		case PokemonType::Dark:
+			
+			break;
+		case PokemonType::Dragon:
+			
+			break;
+		case PokemonType::Steel:
+		
+			break;
+		case PokemonType::Fairy:
+			
+			break;
+		case PokemonType::None:
+			
+			break;
+		default:
+			break;
+		}
+
 		if (move.pokemonTypeOfMove == PokemonType::Normal) {
 			stylesheet = al_load_bitmap("../LeytonFinalProjectC++/Sprites/AttackAnimations/003-Attack01.png");
 			xMaxStyleSheet = 5;
