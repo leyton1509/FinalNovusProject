a5b5df590f1a32d3495aee1bce8ef71511fa5f82
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-06 22:08:21 +0000

    Added a draw display witht he pokemon party

--- LeytonFinalProjectC++/Sprites/NumberOfPokemonDisplay.cpp
+++ LeytonFinalProjectC++/Sprites/NumberOfPokemonDisplay.cpp
@@ -1,3 +1,4 @@
+#include "../Pokemon/Pokemon.h"
 class NumberOfPokemonDisplay {
 
 public:
@@ -11,6 +12,10 @@ public:
 		startX = _startX;
 		startY = _startY;
 		radius = _radius;
+	}
+
+	void drawDisplay(Pokemon party[]) {
+
 	}
 
 
