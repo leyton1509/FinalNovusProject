870c1a4f1fccbeadac5f1e9c8ff01c8f80cf8923
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-06 22:17:52 +0000

    Added comments

--- LeytonFinalProjectC++/Sprites/NumberOfPokemonDisplay.cpp
+++ LeytonFinalProjectC++/Sprites/NumberOfPokemonDisplay.cpp
@@ -1,21 +1,28 @@
 #include "../Pokemon/Pokemon.h"
 #include <allegro5/allegro_primitives.h>
 
+// Displays the party info about what pokemon are dead
 class NumberOfPokemonDisplay {
 
 public:
 
-
+	// The starting x of the pokeball display
 	int startX;
+	// The starting y of the pokeball display
 	int startY;
+	// The radius of each circle
 	int radius;
 
+	// Constructor that takes the position and size of the cricles
 	NumberOfPokemonDisplay(int _startX, int _startY, int _radius) {
 		startX = _startX;
 		startY = _startY;
 		radius = _radius;
 	}
 
+	// Draws the display based on the party
+	// Draws a ligt grey circle
+	// The draws a red circle if its alive, dark grey if its not and a little dot in the middle
 	void drawDisplay(Pokemon party[]) {
 		for (int i = 0; i < 6; i++)
 		{
