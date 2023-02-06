0b9aaeddfd4f39e892c697ba6c6bfb85cdd28459
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-06 11:54:40 +0000

    Added the vars for the animation

--- LeytonFinalProjectC++/Battle/MoveAnimation/MoveAnimation.cpp
+++ LeytonFinalProjectC++/Battle/MoveAnimation/MoveAnimation.cpp
@@ -1,5 +1,14 @@
 class MoveAnimation {
 
+	int currentFrame;
+	int startX;
+	int startY;
+	int destinationX;
+	int destinationY;
+	int state;
+	int xTileForStyleSheet;
+	int yStileForSyleSheet;
+
 	MoveAnimation() {
 
 	}
