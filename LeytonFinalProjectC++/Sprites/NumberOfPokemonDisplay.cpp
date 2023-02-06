ba3aad5960d960389dec5394f89f5ae60fdb3a00
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-06 22:23:26 +0000

    Changed to filled circle

--- LeytonFinalProjectC++/Sprites/NumberOfPokemonDisplay.cpp
+++ LeytonFinalProjectC++/Sprites/NumberOfPokemonDisplay.cpp
@@ -26,16 +26,16 @@ public:
 	void drawDisplay(Pokemon party[]) {
 		for (int i = 0; i < 6; i++)
 		{
-			al_draw_circle((startX * i + 10), startY, radius, al_map_rgb(211,211,211), 1);
+			al_draw_filled_circle((startX * i + 10), startY, radius, al_map_rgb(211,211,211));
 			if (!(strcmp(party[i].pokemonName.c_str(), "") == 0)) {
 
 				if (party[i].currentHealth > 0) {
-					al_draw_circle((startX * i + 10), startY, (radius * 0.8), al_map_rgb(238, 64, 86), 1);
+					al_draw_filled_circle((startX * i + 10), startY, (radius * 0.8), al_map_rgb(238, 64, 86));
 				}
 				else {
-					al_draw_circle((startX * i + 10), startY, (radius * 0.8), al_map_rgb(24, 24, 24), 1);
+					al_draw_filled_circle((startX * i + 10), startY, (radius * 0.8), al_map_rgb(24, 24, 24));
 				}
-				al_draw_circle((startX * i + 10), startY, (radius * 0.1), al_map_rgb(255, 25, 255), 1);
+				al_draw_filled_circle((startX * i + 10), startY, (radius * 0.1), al_map_rgb(255, 25, 255));
 			}
 
 			
