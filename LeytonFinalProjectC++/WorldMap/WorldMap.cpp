246ce10ef6f7bb974b0845009dd3753b864c248d
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-08 11:31:36 +0000

    Added drawing of gym

--- LeytonFinalProjectC++/WorldMap/WorldMap.cpp
+++ LeytonFinalProjectC++/WorldMap/WorldMap.cpp
@@ -687,6 +687,9 @@ public:
 				else if (textMap[i][j] == 20) {
 					al_draw_scaled_bitmap(mapTiles[textMap[i][j]], 0, 0, 159, 145, (0), (0), (32 * 12), (32 * 8), 0);
 				}
+				else if (textMap[i][j] == 32) {
+					al_draw_scaled_bitmap(mapTiles[textMap[i][j]], 0, 0, 86, 65, (i * 32), (j * 32), (32 * 5), (32 * 4), 0);
+				}
 				else {
 					al_draw_bitmap(mapTiles[textMap[i][j]], i * 32, j * 32, 0);
 				}
