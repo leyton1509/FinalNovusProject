5f408f03633c09f2a84cee8d6e0ed29798967f49
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-09 10:48:45 +0000

    Added and fixed items

--- LeytonFinalProjectC++/Sprites/InteractablesForMaps.cpp
+++ LeytonFinalProjectC++/Sprites/InteractablesForMaps.cpp
@@ -13,30 +13,60 @@ public:
 
 		// Map one has items e.g potion pokeball greatball
 		if (mapNumber == 1) {
+			// Breifcase for choosing pokemon
 			Interactable bf = Interactable(1, true, 220, 165, 15 * 32, 24 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/MapSprites/Case.png");
 			interactbles.push_front(bf);
 
+			// Potion
 			Interactable item = Interactable(2, true, 6 * 32, 5 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 15,0);
 			interactbles.push_front(item);
 
+			// Pokeball
 			item = Interactable(5, true, 20 * 32, 22 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 3, 0);
 			interactbles.push_front(item);
 
+			// Super potion
 			item = Interactable(3, true, 18 * 32, 28 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 24, 0);
 			interactbles.push_front(item);
 
+			// Pokeball
 			item = Interactable(5, true, 14 * 32, 44 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 3, 0);
 			interactbles.push_front(item);
 
+			// Great ball
 			item = Interactable(6, true, 14 * 32, 44 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 3, 0);
 			interactbles.push_front(item);
-
+			// Great ball
 			item = Interactable(6, true, 19 * 32, 56 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 3, 0);
 			interactbles.push_front(item);
 
 		
 
 			return interactbles;
+		}
+		// Items for the second map
+		else if(mapNumber == 2)
+		{
+			Interactable bf = Interactable(1, true, 220, 165, 44 * 32, 73 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/MapSprites/Case.png");
+			interactbles.push_front(bf);
+
+			// Potion
+			Interactable item = Interactable(2, true, 27 * 32, 41 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 15, 0);
+			interactbles.push_front(item);
+
+			//Poke ball
+			item = Interactable(5, true, 22 * 32, 15 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 3, 0);
+			interactbles.push_front(item);
+
+			// Super potion
+			item = Interactable(3, true, 25 * 32, 27 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 24, 0);
+			interactbles.push_front(item);
+			// Ultra ball
+			item = Interactable(7, true, 40 * 32, 58 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 1, 0);
+			interactbles.push_front(item);
+
+
+		
 		}
 		// Map 11 is for healing and the box of pokemon
 		else if (mapNumber == 11) {
