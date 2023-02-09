017a5fdd7b0b3c99960c54b326f3f33c08891c2e
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-09 20:15:55 +0000

    Added last pokemon check

--- LeytonFinalProjectC++/Tests/PokemonManagerTests.cpp
+++ LeytonFinalProjectC++/Tests/PokemonManagerTests.cpp
@@ -26,3 +26,31 @@ TEST_F(PokemonManagerTests, CheckFirstPokemon) {
 	ASSERT_EQ(testP.specialDefenceBase, 65);
 	ASSERT_EQ(testP.speedBase, 45);
 }
+
+TEST_F(PokemonManagerTests, CheckSecondToLastPokemon) {
+	Pokemon testP = pokemonManager.getDefaultPokemon("Shaymin");
+	ASSERT_EQ(testP.pokemonName, "Shaymin");
+	ASSERT_EQ(testP.level, 1);
+	testP.setPokemonsLevel(15);
+	ASSERT_EQ(testP.level, 15);
+	ASSERT_EQ(testP.healthBase, 100);
+	ASSERT_EQ(testP.physcialAttackBase, 100);
+	ASSERT_EQ(testP.physicalDefenceBase, 100);
+	ASSERT_EQ(testP.specialAttackBase, 100);
+	ASSERT_EQ(testP.specialDefenceBase, 100);
+	ASSERT_EQ(testP.speedBase, 100);
+}
+
+TEST_F(PokemonManagerTests, CheckToLastPokemon) {
+	Pokemon testP = pokemonManager.getDefaultPokemon("Arceus");
+	ASSERT_EQ(testP.pokemonName, "Arceus");
+	ASSERT_EQ(testP.level, 1);
+	testP.setPokemonsLevel(15);
+	ASSERT_EQ(testP.level, 15);
+	ASSERT_EQ(testP.healthBase, 120);
+	ASSERT_EQ(testP.physcialAttackBase, 120);
+	ASSERT_EQ(testP.physicalDefenceBase, 120);
+	ASSERT_EQ(testP.specialAttackBase, 120);
+	ASSERT_EQ(testP.specialDefenceBase, 120);
+	ASSERT_EQ(testP.speedBase, 120);
+}
