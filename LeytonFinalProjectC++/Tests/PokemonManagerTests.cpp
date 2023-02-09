5f9b784b505134d709d824e9910d0245ca017064
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-09 20:12:31 +0000

    Added test for assering not null for pm

--- LeytonFinalProjectC++/Tests/PokemonManagerTests.cpp
+++ LeytonFinalProjectC++/Tests/PokemonManagerTests.cpp
@@ -8,3 +8,16 @@ protected:
 
 
 };
+
+
+TEST_F(PokemonManagerTests, CheckPokemonManagerInstance) {
+	ASSERT_NE(pokemonManager, NULL);
+}
+TEST_F(PokemonManagerTests, CheckFirstPokemon) {
+	Pokemon testP =  pokemonManager.getDefaultPokemon("Charmander");
+	ASSERT_EQ(testP.pokemonName, "Charmander");
+	ASSERT_EQ(testP.level,1);
+	testP.setPokemonsLevel(5);
+	ASSERT_EQ(testP.level, 5);
+	ASSERT_EQ(testP.pokemonName, "Charmander");
+}
