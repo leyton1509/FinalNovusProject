c2e965ed3bd9ce453fef91b652a2cc581e4154df
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-09 20:26:28 +0000

    Added check for pound

--- LeytonFinalProjectC++/Tests/MoveManagerTests.cpp
+++ LeytonFinalProjectC++/Tests/MoveManagerTests.cpp
@@ -1,9 +1,22 @@
 #include "gtest/gtest.h"
 #include "../Pokemon/Moves/MoveManager.h"
-// Class for testing pokemon manager
+// Class for testing pokemon move manager
 class MoveManagerTests : public ::testing::Test
 {
 
 protected:
 	MoveManager moveManager = moveManager.instance();
 };
+
+// Checks to see if the first move is loaded correctly
+TEST_F(MoveManagerTests, CheckFirstMove) {
+	Move testM = moveManager.getMoveDetails(1);
+	ASSERT_EQ(testM.moveName, "Pound");
+	ASSERT_EQ(testM.power, 40);
+	ASSERT_EQ(testM.accuracy, 100);
+	ASSERT_EQ(testM.powerpoints, 35);
+	ASSERT_EQ(testM.typeOfMove,MoveCategory::Physical);
+	ASSERT_EQ(testM.pokemonTypeOfMove, PokemonType::Normal);
+
+
+}
