b23b3105075c658f0feddf42e9de35e5aff1ac90
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-10 14:07:44 +0000

    Trainer tests all lines of sight

--- LeytonFinalProjectC++/Tests/TrainerTests.cpp
+++ LeytonFinalProjectC++/Tests/TrainerTests.cpp
@@ -30,3 +30,35 @@ TEST_F(TrainerTests, CheckGetTrainer) {
 TEST_F(TrainerTests, CheckGetTrainerForMap) {
 	ASSERT_TRUE(mapHasTrainer(1));
 }
+
+
+// Testing in line of sight function
+TEST_F(TrainerTests, CheckLineOfSight) {
+	Trainer t = trainerManager.getTrainer(1);
+
+	t.setPositions(5, 5, 0, 1, 1);
+	ASSERT_TRUE(t.isInLineOfSight(5, 4));
+	ASSERT_TRUE(t.isInLineOfSight(5, 3));
+	ASSERT_TRUE(t.isInLineOfSight(5, 2));
+	ASSERT_TRUE(t.isInLineOfSight(5, 1));
+	
+
+	t.setPositions(5, 5, 0, -1, 1);
+	ASSERT_TRUE(t.isInLineOfSight(5, 6));
+	ASSERT_TRUE(t.isInLineOfSight(5, 7));
+	ASSERT_TRUE(t.isInLineOfSight(5, 8));
+	ASSERT_TRUE(t.isInLineOfSight(5, 9));
+
+	t.setPositions(5, 5, 1, 0, 1);
+	ASSERT_TRUE(t.isInLineOfSight(6, 5));
+	ASSERT_TRUE(t.isInLineOfSight(7, 5));
+	ASSERT_TRUE(t.isInLineOfSight(8, 5));
+	ASSERT_TRUE(t.isInLineOfSight(9, 5));
+	t.setPositions(5, 5, -1, 0, 1);
+	ASSERT_TRUE(t.isInLineOfSight(4, 5));
+	ASSERT_TRUE(t.isInLineOfSight(3, 5));
+	ASSERT_TRUE(t.isInLineOfSight(2, 5));
+	ASSERT_TRUE(t.isInLineOfSight(1, 5));
+
+	
+}
