fb58fbb4037586c8554584dcd2a5927409fb5486
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-10 13:45:12 +0000

    Added trainer manager

--- LeytonFinalProjectC++/Tests/TrainerTests.cpp
+++ LeytonFinalProjectC++/Tests/TrainerTests.cpp
@@ -1,9 +1,10 @@
 #include "gtest/gtest.h"
+#include "../Sprites/TrainerManager.h"
 
 // Class for testing pokemon manager
 class TrainerTests : public ::testing::Test
 {
 
 protected:
-	
+	TrainerManager trainerManager = TrainerManager();
 };
