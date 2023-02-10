40da24727f1515f61c06ea6d8fd6d90f189ce22c
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-10 11:03:13 +0000

    Added base class

---
+++ LeytonFinalProjectC++/Tests/PlayerCharacterTests.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "../Sprites/PlayerCharacter.h"
+// Class for testing pokemon move manager
+class PlayerCharacterTests : public ::testing::Test
+{
+
+protected:
+	PlayerCharacter playerCharacter = PlayerCharacter();
+};
