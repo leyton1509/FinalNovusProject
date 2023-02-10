58c54080a55b99fce50a20324f7f931491644570
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-10 16:59:16 +0000

    Fixed up interables

--- LeytonFinalProjectC++/Sprites/InteractablesForMaps.cpp
+++ LeytonFinalProjectC++/Sprites/InteractablesForMaps.cpp
@@ -25,15 +25,12 @@ public:
 			item = Interactable(5, true, 20 * 32, 22 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 3, 0);
 			interactbles.push_front(item);
 
-			// Pokeball
-			item = Interactable(5, true, 14 * 32, 45 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 3, 0);
-			interactbles.push_front(item);
 
 			// Great ball
-			item = Interactable(6, true, 14 * 32, 44 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 3, 0);
+			item = Interactable(6, true, 14 * 32, 45 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 2, 0);
 			interactbles.push_front(item);
 			// Great ball
-			item = Interactable(6, true, 19 * 32, 56 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 3, 0);
+			item = Interactable(6, true, 19 * 32, 56 * 32, 32, 32, "../LeytonFinalProjectC++/Sprites/Items/Items.png", 2, 0);
 			interactbles.push_front(item);
 
 		
