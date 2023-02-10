8c8e89fdf1134d603ff8e76e1551810aca510e4d
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-10 12:38:36 +0000

    Changed it so that it can be tested mutliple times

--- LeytonFinalProjectC++/Tests/RouteManagerTests.cpp
+++ LeytonFinalProjectC++/Tests/RouteManagerTests.cpp
@@ -7,26 +7,31 @@ class RouteManagerTests : public ::testing::Test
 
 protected:
 	RouteMapping routeMap = RouteMapping();
+
+	bool checkPokemonFromRouteRandom()
+	{
+		Pokemon p = routeMap.getSinglePokemonFromRoute(1);
+		string availablePokemon[5] = { "Bulbasaur", "Pidgey", "Bidoof", "Charmander", "Squirtle" };
+		bool found = false;
+		for (string& s : availablePokemon)
+		{
+			if (strcmp(s.c_str(), p.pokemonName.c_str()) == 0)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
 };
 
 
-// Checks to see if experience is dependent on level
-TEST_F(RouteManagerTests, CheckPokemonFromRoute) {
-	Pokemon p = routeMap.getSinglePokemonFromRoute(1);
-	string availablePokemon[5] = { "Bulbasaur", "Pidgey", "Bidoof", "Charmander", "Squirtle" };
-	bool found = false;
-	for (string& s : availablePokemon)
-	{
-		if(strcmp(s.c_str(), p.pokemonName.c_str()) == 0)
-		{
-			found = true;
-			ASSERT_EQ(s, p.pokemonName);
-		}
-	}
 
-	if(!found)
-	{
-		ASSERT_FALSE(true);
-	}
+// Checks to see if a random pokemon is got from the route
+TEST_F(RouteManagerTests, CheckPokemonFromRoute) {
+	ASSERT_TRUE(checkPokemonFromRouteRandom());
+	ASSERT_TRUE(checkPokemonFromRouteRandom());
+	ASSERT_TRUE(checkPokemonFromRouteRandom());
+	ASSERT_TRUE(checkPokemonFromRouteRandom());
 	
 }
