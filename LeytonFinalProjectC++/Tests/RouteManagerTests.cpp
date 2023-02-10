43e275437f3b12257e1d39b361155545fa755f17
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-10 12:38:26 +0000

    Added test for getting a random pokemon

--- LeytonFinalProjectC++/Tests/RouteManagerTests.cpp
+++ LeytonFinalProjectC++/Tests/RouteManagerTests.cpp
@@ -8,3 +8,25 @@ class RouteManagerTests : public ::testing::Test
 protected:
 	RouteMapping routeMap = RouteMapping();
 };
+
+
+// Checks to see if experience is dependent on level
+TEST_F(RouteManagerTests, CheckPokemonFromRoute) {
+	Pokemon p = routeMap.getSinglePokemonFromRoute(1);
+	string availablePokemon[5] = { "Bulbasaur", "Pidgey", "Bidoof", "Charmander", "Squirtle" };
+	bool found = false;
+	for (string& s : availablePokemon)
+	{
+		if(strcmp(s.c_str(), p.pokemonName.c_str()) == 0)
+		{
+			found = true;
+			ASSERT_EQ(s, p.pokemonName);
+		}
+	}
+
+	if(!found)
+	{
+		ASSERT_FALSE(true);
+	}
+	
+}
