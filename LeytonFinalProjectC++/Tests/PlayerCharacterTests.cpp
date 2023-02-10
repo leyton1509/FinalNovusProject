e0e96d76d4a206a1d9c0d1f51fa043867c0e6a28
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-10 11:15:11 +0000

    Added check for s clicked

--- LeytonFinalProjectC++/Tests/PlayerCharacterTests.cpp
+++ LeytonFinalProjectC++/Tests/PlayerCharacterTests.cpp
@@ -8,7 +8,7 @@ protected:
 	PlayerCharacter playerCharacter = PlayerCharacter();
 };
 
-// Checks to see if the first move is loaded correctly
+// Checks to see if the players tile location is changed when moved
 TEST_F(PlayerCharacterTests, CheckMoveW) {
 	int beforeXLoc = playerCharacter.xTilePosition;
 	int beforeYLoc = playerCharacter.yTilePosition;
@@ -20,5 +20,18 @@ TEST_F(PlayerCharacterTests, CheckMoveW) {
 	int afterYLoc = playerCharacter.yTilePosition;
 	ASSERT_EQ(beforeXLoc, afterXLoc);
 	ASSERT_EQ((beforeYLoc-1), afterYLoc);
+}
 
+// Checks to see if the players tile location is changed when moved
+TEST_F(PlayerCharacterTests, CheckMoveS) {
+	int beforeXLoc = playerCharacter.xTilePosition;
+	int beforeYLoc = playerCharacter.yTilePosition;
+	playerCharacter.directionX = 0;
+	playerCharacter.directionY = -1;
+	playerCharacter.moveCharacter("s");
+	playerCharacter.updateTile();
+	int afterXLoc = playerCharacter.xTilePosition;
+	int afterYLoc = playerCharacter.yTilePosition;
+	ASSERT_EQ(beforeXLoc, afterXLoc);
+	ASSERT_EQ((beforeYLoc +1), afterYLoc);
 }
