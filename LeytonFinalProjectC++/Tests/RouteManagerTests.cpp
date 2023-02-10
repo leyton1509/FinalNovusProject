c8a797c6d96feda92c7669cfb4a02dcb719a6f61
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-10 12:30:56 +0000

    Added a class for route mapping check

--- LeytonFinalProjectC++/Tests/RouteManagerTests.cpp
+++ LeytonFinalProjectC++/Tests/RouteManagerTests.cpp
@@ -1,1 +1,10 @@
-﻿
+﻿#include "gtest/gtest.h"
+#include "../Battle/RouteMapping.h"
+
+// Class for testing pokemon move manager
+class RouteManagerTests : public ::testing::Test
+{
+
+protected:
+	RouteMapping routeMap = RouteMapping();
+};
