4bfaf6904cb38c8e295780034f5c596187c92f6d
leyton1509 <taylor92@lancaster.ac.uk>
2023-02-10 11:14:18 +0000

    Added check for w move

--- LeytonFinalProjectC++/Tests/PlayerCharacterTests.cpp
+++ LeytonFinalProjectC++/Tests/PlayerCharacterTests.cpp
@@ -7,3 +7,18 @@ class PlayerCharacterTests : public ::testing::Test
 protected:
 	PlayerCharacter playerCharacter = PlayerCharacter();
 };
+
+// Checks to see if the first move is loaded correctly
+TEST_F(PlayerCharacterTests, CheckMoveW) {
+	int beforeXLoc = playerCharacter.xTilePosition;
+	int beforeYLoc = playerCharacter.yTilePosition;
+	playerCharacter.directionX = 0;
+	playerCharacter.directionY = 1;
+	playerCharacter.moveCharacter("w");
+	playerCharacter.updateTile();
+	int afterXLoc = playerCharacter.xTilePosition;
+	int afterYLoc = playerCharacter.yTilePosition;
+	ASSERT_EQ(beforeXLoc, afterXLoc);
+	ASSERT_EQ((beforeYLoc-1), afterYLoc);
+
+}
